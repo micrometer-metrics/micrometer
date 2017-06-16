4b09ec134f78d1a02c15fb6995054cddcaeed71f
Jon Schneider <jkschneider@gmail.com>
2017-06-16 17:01:17 -0500

    Fix release versioning

--- gradle/ciBuild.sh
+++ gradle/ciBuild.sh
@@ -10,13 +10,14 @@ elif [ -z $CIRCLE_TAG ]; then
   echo -e ?'Build Branch with Snapshot => Branch ['$CIRCLE_BRANCH']'
   ./gradlew clean build $SWITCHES
 elif [ $CIRCLE_TAG ]; then
-  echo -e 'Build Branch for Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']'
+  RELEASE_VERSION=$(echo $CIRCLE_TAG | sed 's/^v\(.*\)/\1/')
+  echo -e 'Build Branch for Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG'] Version ['$RELEASE_VERSION']'
   case "$CIRCLE_TAG" in
   *-rc\.*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.version=$CIRCLE_TAG clean build candidate $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.version=$RELEASE_VERSION clean build candidate $SWITCHES
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.version=$CIRCLE_TAG clean build final $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.version=$RELEASE_VERSION clean build final $SWITCHES
     ;;
   esac
 else
