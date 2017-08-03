76387689cf77c526e058888651203dcd7bc20000
Clint Checketts <clint.checketts@domo.com>
2017-08-03 00:15:02 -0600

    Add Tests and move get/set registry into inner Config class to mask details

--- micrometer-core/src/main/java/io/micrometer/core/instrument/MeterFactory.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/MeterFactory.java
@@ -2,30 +2,24 @@ package io.micrometer.core.instrument;
 
 import io.micrometer.core.instrument.noop.NoOpRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MeterFactory {
-  private static final Logger LOGGER = LoggerFactory.getLogger(MeterFactory.class);
-
-  private static AtomicReference<MeterRegistry> registry = new AtomicReference<>(new SimpleMeterRegistry());
-
   public static Counter counter(String name, String... tags){
-    return registry.get().counter(name, tags);
+    return Config.registry.get().counter(name, tags);
   }
 
   public static LongTaskTimer longTaskTimer(String name, String... tags){
-    return registry.get().longTaskTimer(name, tags);
+    return Config.registry.get().longTaskTimer(name, tags);
   }
 
   public static DistributionSummary summary(String name, String... tags){
-    return registry.get().summary(name, tags);
+    return Config.registry.get().summary(name, tags);
   }
 
   public static Timer timer(String name, String... tags){
-    return registry.get().timer(name, tags);
+    return Config.registry.get().timer(name, tags);
   }
 
 //  public Gauge gauge(String name, DoubleSupplier gaugeProvider, String... tags){
@@ -38,19 +32,23 @@ public class MeterFactory {
 //  }
 
   public static Meter meter(Meter meter){
-    registry.get().register(meter);
+    Config.registry.get().register(meter);
     return meter;
   }
 
-  public static void setRegistry(MeterRegistry newRegistry){
-    if(newRegistry == null) {
-      throw new NullPointerException("Null Registry is not allowed. Use a "+ NoOpRegistry.class.getCanonicalName()+" instead");
-    }
-    registry.set(newRegistry);
-  }
+  public static class Config {
+    private static AtomicReference<MeterRegistry> registry = new AtomicReference<>(new SimpleMeterRegistry());
 
-  public static MeterRegistry registry() {
-    return registry.get();
+    public static void setRegistry(MeterRegistry newRegistry){
+      if(newRegistry == null) {
+        throw new NullPointerException("Null Registry is not allowed. Use a "+ NoOpRegistry.class.getCanonicalName()+" instead");
+      }
+      registry.set(newRegistry);
+    }
+
+    public static MeterRegistry registry() {
+      return registry.get();
+    }
   }
 
 }
---
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/MeterFactoryTest.java
@@ -0,0 +1,62 @@
+package io.micrometer.core.instrument;
+
+import io.micrometer.core.instrument.noop.NoOpRegistry;
+import io.micrometer.core.instrument.prometheus.PrometheusMeterRegistry;
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class MeterFactoryTest {
+  private MeterRegistry registry;
+
+  @ParameterizedTest
+  @ArgumentsSource(MeterRegistriesProvider.class)
+  @DisplayName("meters with the same name and tags are registered once")
+  void uniqueMeters(MeterRegistry registry) {
+    MeterFactory.Config.setRegistry(registry);
+
+    MeterFactory.counter("foo");
+    MeterFactory.counter("foo");
+
+    assertThat(MeterFactory.Config.registry().getMeters().size()).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("no op registry is supported and uses the same internal meters to minimize memory impact")
+  void noopMeter() {
+    MeterFactory.Config.setRegistry(new NoOpRegistry());
+
+    Counter a = MeterFactory.counter("foo");
+    a.increment();
+    Counter b = MeterFactory.counter("foo2");
+    b.increment(10D);
+    assertThat(a).isSameAs(b);
+
+    assertThat(MeterFactory.Config.registry().getMeters().size())
+            .as("NoOpRegistry is always empty")
+            .isEqualTo(0);
+  }
+
+  @BeforeEach
+  void init() {
+    //Save off existing registry
+    registry = MeterFactory.Config.registry();
+  }
+
+  @AfterEach
+  void tearDown() {
+    //Reset registry
+    MeterFactory.Config.setRegistry(registry);
+  }
+
+
+
+
+}
