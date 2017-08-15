2ace97dd1296f348445baa27838ccdeb0513f7f3
Clint Checketts <clint.checketts@domo.com>
2017-08-15 11:26:45 -0600

    Add in the Inject dep

--- micrometer-spring-legacy/src/samples/java/io/micrometer/spring/samples/library/ProposalLibrary.java
+++ micrometer-spring-legacy/src/samples/java/io/micrometer/spring/samples/library/ProposalLibrary.java
@@ -4,10 +4,11 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Meters;
 
+import javax.inject.Inject;
+
 public class ProposalLibrary {
 
-    //@Inject no available on classpath
-    MeterRegistry registry = MeterRegistry.globalRegistry;
+    @Inject MeterRegistry registry = MeterRegistry.globalRegistry;
     Counter workCounter = Meters.lazyCounter(() -> registry.counter("work_counter"));
 
     public void doWork() {
