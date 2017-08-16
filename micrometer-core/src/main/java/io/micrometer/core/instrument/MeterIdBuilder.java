4f8a541024bd417a4229e8d68e842fa62e1a6a39
Clint Checketts <clint.checketts@domo.com>
2017-08-15 23:47:50 -0600

    Add remaining methods to match registry

--- micrometer-core/src/main/java/io/micrometer/core/instrument/MeterIdBuilder.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/MeterIdBuilder.java
@@ -16,43 +16,90 @@
 package io.micrometer.core.instrument;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.ToDoubleFunction;
 
+import static java.util.Collections.emptyList;
+
 public class MeterIdBuilder {
 
-  private String name;
-  private List<Tag> tags;
-  private MeterRegistry registry;
+    private String name;
+    private List<Tag> tags;
+    private MeterRegistry registry;
 
-  public MeterIdBuilder(String name, MeterRegistry registry) {
-    this.name = name;
-    this.registry = registry;
-    this.tags = new ArrayList<>();
-  }
-
-  public MeterIdBuilder tag(String key, String value) {
-    tags.add(new ImmutableTag(key, value));
-    return this;
-  }
-
-  public Counter counter(){
-    return registry.counter(name, tags);
-  }
-
-  public <T> T gauge(T obj, ToDoubleFunction<T> f){
-    return registry.gauge(name, tags, obj, f);
-  }
-
-  public MeterIdBuilder tags(Iterable<Tag> moreTags) {
-    for(Tag t : moreTags) {
-      tags.add(t);
+    public MeterIdBuilder(String name, MeterRegistry registry) {
+        this.name = name;
+        this.registry = registry;
+        this.tags = new ArrayList<>();
     }
-    return this;
-  }
 
-  public AtomicLong counter(AtomicLong atomicLong) {
-    return registry.counter(name, tags, atomicLong);
-  }
+    public MeterIdBuilder tag(String key, String value) {
+        tags.add(new ImmutableTag(key, value));
+        return this;
+    }
+
+    public MeterIdBuilder tags(Iterable<Tag> moreTags) {
+        for (Tag t : moreTags) {
+            tags.add(t);
+        }
+        return this;
+    }
+
+
+    public Counter counter() {
+        return registry.counter(name, tags);
+    }
+
+    public AtomicLong counter(AtomicLong atomicLong) {
+        return registry.counter(name, tags, atomicLong);
+    }
+
+
+    public <T> Gauge gauge(T obj, ToDoubleFunction<T> f) {
+        return registry.gaugeBuilder(name, obj, f).tags(tags).create();
+    }
+
+    /**
+     * Build a new Distribution Summary, which is registered with this registry once {@link DistributionSummary.Builder#create()} is called.
+     *
+     * @return The builder.
+     */
+    DistributionSummary.Builder summaryBuilder(){
+        return registry.summaryBuilder(name);
+    }
+
+
+    /**
+     * Measures the sample distribution of events.
+     */
+    public DistributionSummary summary() {
+        return registry.summary(name, tags);
+    }
+
+    /**
+     * Build a new Timer, which is registered with this registry once {@link Timer.Builder#create()} is called.
+     *
+     * @return The builder.
+     */
+    Timer.Builder timerBuilder() {
+        return registry.timerBuilder(name);
+    }
+
+    /**
+     * Measures the time taken for short tasks.
+     */
+    public Timer timer() {
+        return timerBuilder().tags(tags).create();
+    }
+
+    /**
+     * Measures the time taken for short tasks.
+     */
+    LongTaskTimer longTaskTimer() {
+        return registry.longTaskTimer(name, tags);
+    }
+
 }
