403861a5192a25e8d4d553534aa4e15529d9c159
Jon Schneider <jkschneider@gmail.com>
2017-09-16 17:49:04 -0500

    Add datadog sample

---
+++ micrometer-spring-legacy/src/samples/java/io/micrometer/spring/samples/DatadogSample.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2017 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.spring.samples;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages = "io.micrometer.spring.samples.components")
+@EnableScheduling
+public class DatadogSample {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(DatadogSample.class).profiles("datadog").run(args);
+    }
+}
