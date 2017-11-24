79640b0a07da4346643d63c3971353dcdc96c4a9
Jon Schneider <jkschneider@gmail.com>
2017-11-23 23:21:26 -0600

    Conditionalize web mvc metrics by @ConditionalOnWebApplication (fixes #244)

--- micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/web/servlet/WebMvcMetricsConfiguration.java
+++ micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/web/servlet/WebMvcMetricsConfiguration.java
@@ -23,6 +23,7 @@ import io.micrometer.spring.web.servlet.WebMvcMetrics;
 import io.micrometer.spring.web.servlet.WebMvcTagsProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +38,7 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
  */
 @Configuration
 @ConditionalOnClass(DispatcherServlet.class)
+@ConditionalOnWebApplication
 @EnableConfigurationProperties(MetricsProperties.class)
 public class WebMvcMetricsConfiguration {
 
