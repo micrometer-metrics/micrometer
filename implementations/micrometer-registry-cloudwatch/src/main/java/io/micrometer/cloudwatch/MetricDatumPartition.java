7f435d64daf51977e1b6161e4e94f2e9e17d65ac
Johnny Lim <izeye@naver.com>
2017-12-14 18:14:37 +0900

    Remove IntMath usage in MeterPartition

--- implementations/micrometer-registry-cloudwatch/src/main/java/io/micrometer/cloudwatch/MetricDatumPartition.java
+++ implementations/micrometer-registry-cloudwatch/src/main/java/io/micrometer/cloudwatch/MetricDatumPartition.java
@@ -16,13 +16,15 @@
 package io.micrometer.cloudwatch;
 
 import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import io.micrometer.core.instrument.util.MathUtils;
 
-import java.math.RoundingMode;
 import java.util.AbstractList;
 import java.util.List;
 
 /**
- * Modified from {@link io.micrometer.core.instrument.util.MeterPartition}
+ * Modified from {@link io.micrometer.core.instrument.util.MeterPartition}.
+ *
+ * @author Dawid Kublik
  */
 public class MetricDatumPartition extends AbstractList<List<MetricDatum>> {
     private final List<MetricDatum> list;
@@ -42,33 +44,7 @@ public class MetricDatumPartition extends AbstractList<List<MetricDatum>> {
 
     @Override
     public int size() {
-        return divideWithCeilingRoundingMode(list.size(), size);
-    }
-
-    /**
-     * simplified {@link com.google.common.math.IntMath#divide(int, int, RoundingMode)}
-     */
-    private int divideWithCeilingRoundingMode(int p, int q) {
-        if (q == 0) {
-            throw new ArithmeticException("/ by zero"); // for GWT
-        }
-        int div = p / q;
-        int rem = p - q * div; // equal to p % q
-
-        if (rem == 0) {
-            return div;
-        }
-
-        /*
-         * Normal Java division rounds towards 0, consistently with RoundingMode.DOWN. We just have to
-         * deal with the cases where rounding towards 0 is wrong, which typically depends on the sign of
-         * p / q.
-         *
-         * signum is 1 if p and q are both nonnegative or both negative, and -1 otherwise.
-         */
-        int signum = 1 | ((p ^ q) >> (Integer.SIZE - 1));
-        boolean increment = signum > 0;
-        return increment ? div + signum : div;
+        return MathUtils.divideWithCeilingRoundingMode(list.size(), size);
     }
 
     @Override
