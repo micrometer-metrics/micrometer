4191312cdc90a17536b761ecb80ff30bc71c22bc
Sergey Ivanov <sergey@weareadaptive.com>
2018-02-12 12:14:25 -0600

    Added auto-configuration for ClassLoaderMetrics (fixes #418)

--- micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/MeterBindersConfiguration.java
+++ micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/MeterBindersConfiguration.java
@@ -16,6 +16,7 @@
 package io.micrometer.spring.autoconfigure;
 
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
@@ -37,24 +38,31 @@ import org.springframework.context.annotation.Configuration;
 class MeterBindersConfiguration {
 
     @Bean
-    @ConditionalOnProperty(value = "management.metrics.binders.jvm.enabled", matchIfMissing = true)
+    @ConditionalOnProperty(value = "management.metrics.binders.jvm.gc.enabled", matchIfMissing = true)
     @ConditionalOnMissingBean
     public JvmGcMetrics jvmGcMetrics() {
         return new JvmGcMetrics();
     }
 
     @Bean
-    @ConditionalOnProperty(value = "management.metrics.binders.jvm.enabled", matchIfMissing = true)
+    @ConditionalOnProperty(value = "management.metrics.binders.jvm.memory.enabled", matchIfMissing = true)
     @ConditionalOnMissingBean
     public JvmMemoryMetrics jvmMemoryMetrics() {
         return new JvmMemoryMetrics();
     }
 
     @Bean
-    @ConditionalOnProperty(value = "management.metrics.binders.jvm.enabled", matchIfMissing = true)
+    @ConditionalOnProperty(value = "management.metrics.binders.jvm.thread.enabled", matchIfMissing = true)
     @ConditionalOnMissingBean
     public JvmThreadMetrics jvmThreadMetrics() {
         return new JvmThreadMetrics();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "management.metrics.binders.jvm.classes.enabled", matchIfMissing = true)
+    @ConditionalOnMissingBean
+    public ClassLoaderMetrics classLoaderMetrics() {
+        return new ClassLoaderMetrics();
     }
 
     @Bean
