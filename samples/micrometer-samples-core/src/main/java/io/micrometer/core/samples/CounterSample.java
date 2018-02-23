e4b20a609e0f0bed75dffa659bc85742cff982e4
Jon Schneider <jschneider@pivotal.io>
2018-02-23 10:48:52 -0600

    Add Boot 1.5 Statsd Etsy sample

--- samples/micrometer-samples-core/src/main/java/io/micrometer/core/samples/CounterSample.java
+++ samples/micrometer-samples-core/src/main/java/io/micrometer/core/samples/CounterSample.java
@@ -20,6 +20,7 @@ import cern.jet.random.engine.MersenneTwister64;
 import cern.jet.random.engine.RandomEngine;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.samples.utils.SampleConfig;
 import reactor.core.publisher.Flux;
 
@@ -31,10 +32,10 @@ import static java.util.Collections.emptyList;
 public class CounterSample {
     public static void main(String[] args) {
         MeterRegistry registry = SampleConfig.myMonitoringSystem();
-        Counter counter = registry.counter("counter");
+        Counter counter = registry.counter("counter", "method", "actual");
 
         AtomicInteger n = new AtomicInteger(0);
-        registry.more().counter("fcounter", emptyList(), n);
+        registry.more().counter("counter", Tags.of("method", "function"), n);
 
         RandomEngine r = new MersenneTwister64(0);
         Normal dist = new Normal(0, 1, r);
