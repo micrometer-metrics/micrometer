1005c366ea06b9759a3d09fe33b5d5265996ff1c
Aleksander Bartnikiewicz <aleksander@bartnikiewi.cz>
2018-02-24 22:54:47 +0100

    fix checkstyle: removed unused imports

--- implementations/micrometer-registry-influx/src/main/java/io/micrometer/influx/InfluxMeterRegistry.java
+++ implementations/micrometer-registry-influx/src/main/java/io/micrometer/influx/InfluxMeterRegistry.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 import java.util.zip.GZIPOutputStream;
 
