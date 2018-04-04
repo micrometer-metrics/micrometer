0756fc66cbfe4cb270b49ae969aee63edbd7993c
Jon Schneider <jschneider@pivotal.io>
2018-04-03 20:32:51 -0500

    Polish

--- micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/MeterRegistryConfigurer.java
+++ micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/MeterRegistryConfigurer.java
@@ -20,15 +20,14 @@ import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.ResolvableType;
 
 import java.util.Collection;
 import java.util.Collections;
 
 /**
- * {@link BeanPostProcessor} to apply {@link MeterRegistryCustomizer customizers},
- * {@link MeterFilter filters}, {@link MeterBinder binders} and {@link Metrics#addRegistry
+ * Applies {@link MeterRegistryCustomizer customizers}, {@link MeterFilter filters},
+ * {@link MeterBinder binders} and {@link Metrics#addRegistry
  * global registration} to {@link MeterRegistry meter registries}.
  *
  * @author Jon Schneider
@@ -36,59 +35,59 @@ import java.util.Collections;
  */
 class MeterRegistryConfigurer {
 
-	private final Collection<MeterRegistryCustomizer<?>> customizers;
+    private final Collection<MeterRegistryCustomizer<?>> customizers;
 
-	private final Collection<MeterFilter> filters;
+    private final Collection<MeterFilter> filters;
 
-	private final Collection<MeterBinder> binders;
+    private final Collection<MeterBinder> binders;
 
-	private final boolean addToGlobalRegistry;
+    private final boolean addToGlobalRegistry;
 
-	MeterRegistryConfigurer(Collection<MeterBinder> binders,
+    MeterRegistryConfigurer(Collection<MeterBinder> binders,
                             Collection<MeterFilter> filters,
                             Collection<MeterRegistryCustomizer<?>> customizers,
                             boolean addToGlobalRegistry) {
-		this.binders = (binders != null ? binders : Collections.emptyList());
-		this.filters = (filters != null ? filters : Collections.emptyList());
-		this.customizers = (customizers != null ? customizers : Collections.emptyList());
-		this.addToGlobalRegistry = addToGlobalRegistry;
-	}
+        this.binders = (binders != null ? binders : Collections.emptyList());
+        this.filters = (filters != null ? filters : Collections.emptyList());
+        this.customizers = (customizers != null ? customizers : Collections.emptyList());
+        this.addToGlobalRegistry = addToGlobalRegistry;
+    }
 
-	void configure(MeterRegistry registry) {
-		if (registry instanceof CompositeMeterRegistry) {
-			return;
-		}
-		// Customizers must be applied before binders, as they may add custom
-		// tags or alter timer or summary configuration.
-		customize(registry);
-		addFilters(registry);
-		addBinders(registry);
-		if (this.addToGlobalRegistry && registry != Metrics.globalRegistry) {
-			Metrics.addRegistry(registry);
-		}
-	}
+    void configure(MeterRegistry registry) {
+        if (registry instanceof CompositeMeterRegistry) {
+            return;
+        }
+        // Customizers must be applied before binders, as they may add custom
+        // tags or alter timer or summary configuration.
+        customize(registry);
+        addFilters(registry);
+        addBinders(registry);
+        if (this.addToGlobalRegistry && registry != Metrics.globalRegistry) {
+            Metrics.addRegistry(registry);
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private void customize(MeterRegistry registry) {
-		// Customizers must be applied before binders, as they may add custom tags or alter
-		// timer or summary configuration.
-		for (MeterRegistryCustomizer customizer : this.customizers) {
-			Class<?> generic = ResolvableType
-					.forClass(MeterRegistryCustomizer.class, customizer.getClass())
-					.resolveGeneric();
-			if (generic.isAssignableFrom(registry.getClass())) {
-				//noinspection unchecked
-				customizer.customize(registry);
-			}
-		}
-	}
+    @SuppressWarnings("unchecked")
+    private void customize(MeterRegistry registry) {
+        // Customizers must be applied before binders, as they may add custom tags or alter
+        // timer or summary configuration.
+        for (MeterRegistryCustomizer customizer : this.customizers) {
+            Class<?> generic = ResolvableType
+                    .forClass(MeterRegistryCustomizer.class, customizer.getClass())
+                    .resolveGeneric();
+            if (generic.isAssignableFrom(registry.getClass())) {
+                //noinspection unchecked
+                customizer.customize(registry);
+            }
+        }
+    }
 
-	private void addFilters(MeterRegistry registry) {
-		this.filters.forEach(registry.config()::meterFilter);
-	}
+    private void addFilters(MeterRegistry registry) {
+        this.filters.forEach(registry.config()::meterFilter);
+    }
 
-	private void addBinders(MeterRegistry registry) {
-		this.binders.forEach((binder) -> binder.bindTo(registry));
-	}
+    private void addBinders(MeterRegistry registry) {
+        this.binders.forEach((binder) -> binder.bindTo(registry));
+    }
 
 }
