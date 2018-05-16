0a6988b0228323e800aaa79b7bed2641ccfdcfc1
vgaddavcg <vgadda@meetveracity.com>
2018-05-16 11:41:06 -0400

    stops updating metrics when maximumAllowableTags is reached (#603)
    * Update MeterFilter.java
    
    stops updating metrics when maximumAllowableTags is reached
    
    * Update MeterFilterTest.java
    
    stops updating metrics when maximumAllowableTags is reached

--- micrometer-core/src/test/java/io/micrometer/core/instrument/MeterFilterTest.java
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/MeterFilterTest.java
@@ -116,7 +116,8 @@ class MeterFilterTest {
 
         Meter.Id id = new Meter.Id("name", Tags.of("k", "1"), null, null, Meter.Type.COUNTER);
         Meter.Id id2 = new Meter.Id("name", Tags.of("k", "2"), null, null, Meter.Type.COUNTER);
-        Meter.Id id3 = new Meter.Id("name", Tags.of("k", "3"), null, null, Meter.Type.COUNTER);
+        Meter.Id id3 = new Meter.Id("name", Tags.of("k", "3"), null, null, Meter.Type.COUNTER); 
+        Meter.Id id4 = new Meter.Id("anotherName", Tags.of("tag", "4"), null, null, Meter.Type.COUNTER);
 
         filter.accept(id);
         filter.accept(id);
@@ -124,6 +125,9 @@ class MeterFilterTest {
         filter.accept(id);
         filter.accept(id3);
 
+        assertThat(n.get()).isEqualTo(1);
+        
+        filter.accept(id4);
         assertThat(n.get()).isEqualTo(1);
     }
 
