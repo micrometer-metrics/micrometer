5ee6d7abe8e65488d1f7c94b04de4398fe201e12
Vladyslav Oleniuk <vladyslav.oleniuk@kiwigrid.com>
2018-07-06 21:37:51 +0200

    add javadoc to test class

--- implementations/micrometer-registry-influx/src/test/java/io/micrometer/influx/CreateDatabaseQueryBuilderTest.java
+++ implementations/micrometer-registry-influx/src/test/java/io/micrometer/influx/CreateDatabaseQueryBuilderTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Test for {@link CreateDatabaseQueryBuilder}
+ */
 public class CreateDatabaseQueryBuilderTest {
 
 	/**
