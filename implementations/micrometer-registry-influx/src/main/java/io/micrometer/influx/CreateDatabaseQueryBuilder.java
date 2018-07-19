111e0d0184da5edeb8384f859639b805f203a258
Jon Schneider <jschneider@pivotal.io>
2018-07-18 20:50:38 -0500

    Polish and licenses

--- implementations/micrometer-registry-influx/src/main/java/io/micrometer/influx/CreateDatabaseQueryBuilder.java
+++ implementations/micrometer-registry-influx/src/main/java/io/micrometer/influx/CreateDatabaseQueryBuilder.java
@@ -1,85 +1,95 @@
+/**
+ * Copyright 2017 Pivotal Software, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micrometer.influx;
 
-import java.util.*;
+import io.micrometer.core.lang.Nullable;
+
+import java.util.Objects;
 import java.util.stream.Stream;
 
 /**
  * Builds a create database query for influxdb. It is supposed to be of the following structure:
- *
+ * <p>
  * CREATE DATABASE <database_name> [WITH [DURATION <duration>] [REPLICATION <n>] [SHARD DURATION <duration>] [NAME
  * <retention-policy-name>]]
  *
  * @author Vladyslav Oleniuk (vlad.oleniuk@gmail.com)
  */
-public class CreateDatabaseQueryBuilder {
+class CreateDatabaseQueryBuilder {
 
-	private static String QUERY_MANDATORY_TEMPLATE = "CREATE DATABASE %s";
+    private static final String QUERY_MANDATORY_TEMPLATE = "CREATE DATABASE %s";
+    private static final String RETENTION_POLICY_INTRODUCTION = " WITH";
+    private static final String DURATION_CLAUSE_TEMPLATE = " DURATION %s";
+    private static final String REPLICATION_FACTOR_CLAUSE_TEMPLATE = " REPLICATION %s";
+    private static final String SHARD_DURATION_CLAUSE_TEMPLATE = " SHARD DURATION %s";
+    private static final String NAME_CLAUSE_TEMPLATE = " NAME %s";
 
-	private static String RETENTION_POLICY_INTRODUCTION = " WITH";
+    private String databaseName;
+    private String[] retentionPolicyClauses = new String[4];
 
-	private static String DURATION_CLAUSE_TEMPLATE = " DURATION %s";
+    CreateDatabaseQueryBuilder(@Nullable String databaseName) {
+        if (isEmpty(databaseName)) {
+            throw new IllegalArgumentException("The database name cannot be null or empty");
+        }
+        this.databaseName = databaseName;
+    }
 
-	private static String REPLICATION_FACTOR_CLAUSE_TEMPLATE = " REPLICATION %s";
+    CreateDatabaseQueryBuilder setRetentionDuration(@Nullable String retentionDuration) {
+        if (!isEmpty(retentionDuration)) {
+            retentionPolicyClauses[0] = String.format(DURATION_CLAUSE_TEMPLATE, retentionDuration);
+        }
+        return this;
+    }
 
-	private static String SHARD_DURATION_CLAUSE_TEMPLATE = " SHARD DURATION %s";
+    CreateDatabaseQueryBuilder setRetentionReplicationFactor(@Nullable String retentionReplicationFactor) {
+        if (!isEmpty(retentionReplicationFactor)) {
+            retentionPolicyClauses[1] = String.format(REPLICATION_FACTOR_CLAUSE_TEMPLATE, retentionReplicationFactor);
+        }
+        return this;
+    }
 
-	private static String NAME_CLAUSE_TEMPLATE = " NAME %s";
+    CreateDatabaseQueryBuilder setRetentionShardDuration(@Nullable String retentionShardDuration) {
+        if (!isEmpty(retentionShardDuration)) {
+            retentionPolicyClauses[2] = String.format(SHARD_DURATION_CLAUSE_TEMPLATE, retentionShardDuration);
+        }
+        return this;
+    }
 
-	private String databaseName;
+    CreateDatabaseQueryBuilder setRetentionPolicyName(@Nullable String retentionPolicyName) {
+        if (!isEmpty(retentionPolicyName)) {
+            retentionPolicyClauses[3] = String.format(NAME_CLAUSE_TEMPLATE, retentionPolicyName);
+        }
+        return this;
+    }
 
-	private String[] retentionPolicyClauses = new String[4];
+    String build() {
+        StringBuilder queryStringBuilder = new StringBuilder(String.format(QUERY_MANDATORY_TEMPLATE, databaseName));
+        if (toCreateRetentionPolicy()) {
+            String retentionPolicyClause = Stream.of(retentionPolicyClauses).filter(Objects::nonNull)
+                    .reduce(RETENTION_POLICY_INTRODUCTION, String::concat);
+            queryStringBuilder.append(retentionPolicyClause);
+        }
+        return queryStringBuilder.toString();
+    }
 
-	public CreateDatabaseQueryBuilder(String databaseName) {
-		if (isEmpty(databaseName)) {
-			throw new IllegalArgumentException("The database name mustn't be empty");
-		}
-		this.databaseName = databaseName;
-	}
+    private boolean toCreateRetentionPolicy() {
+        return Stream.of(retentionPolicyClauses).anyMatch(Objects::nonNull);
+    }
 
-	public CreateDatabaseQueryBuilder setRetentionDuration(String retentionDuration) {
-		if (!isEmpty(retentionDuration)) {
-			retentionPolicyClauses[0] = String.format(DURATION_CLAUSE_TEMPLATE, retentionDuration);
-		}
-		return this;
-	}
-
-	public CreateDatabaseQueryBuilder setRetentionReplicationFactor(String retentionReplicationFactor) {
-		if (!isEmpty(retentionReplicationFactor)) {
-			retentionPolicyClauses[1] = String.format(REPLICATION_FACTOR_CLAUSE_TEMPLATE, retentionReplicationFactor);
-		}
-		return this;
-	}
-
-	public CreateDatabaseQueryBuilder setRetentionShardDuration(String retentionShardDuration) {
-		if (!isEmpty(retentionShardDuration)) {
-			retentionPolicyClauses[2] = String.format(SHARD_DURATION_CLAUSE_TEMPLATE, retentionShardDuration);
-		}
-		return this;
-	}
-
-	public CreateDatabaseQueryBuilder setRetentionPolicyName(String retentionPolicyName) {
-		if (!isEmpty(retentionPolicyName)) {
-			retentionPolicyClauses[3] = String.format(NAME_CLAUSE_TEMPLATE, retentionPolicyName);
-		}
-		return this;
-	}
-
-	public String build() {
-		StringBuilder queryStringBuilder = new StringBuilder(String.format(QUERY_MANDATORY_TEMPLATE, databaseName));
-		if (toCreateRetentionPolicy()) {
-			String retentionPolicyClause = Stream.of(retentionPolicyClauses).filter(Objects::nonNull)
-					.reduce(RETENTION_POLICY_INTRODUCTION, String::concat);
-			queryStringBuilder.append(retentionPolicyClause);
-		}
-		return queryStringBuilder.toString();
-	}
-
-	private boolean toCreateRetentionPolicy() {
-		return Stream.of(retentionPolicyClauses).anyMatch(Objects::nonNull);
-	}
-
-	private boolean isEmpty(String string) {
-		return string == null || string.isEmpty();
-	}
-
+    private boolean isEmpty(@Nullable String string) {
+        return string == null || string.isEmpty();
+    }
 }
