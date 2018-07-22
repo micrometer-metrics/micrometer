eb20e84c5033283c7383bba64887fc8480f942e7
Anton Ilinchik <anton.ilinchik@zalando.de>
2018-07-22 20:13:13 +0200

    KairosDB support (issue #761)

---
+++ micrometer-spring-legacy/src/main/java/io/micrometer/spring/autoconfigure/export/kairos/KairosProperties.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2017 Pivotal Software, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.spring.autoconfigure.export.kairos;
+
+import io.micrometer.spring.autoconfigure.export.StepRegistryProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * {@link ConfigurationProperties} for configuring Kairos metrics export.
+ *
+ * @author Anton Ilinchik
+ */
+@ConfigurationProperties(prefix = "management.metrics.export.kairos")
+public class KairosProperties extends StepRegistryProperties {
+
+    /**
+     * The host to send the metrics to
+     */
+    private String host;
+
+    /**
+     * The Basic Authentication username.
+     */
+    private String userName;
+
+    /**
+     * The Basic Authentication password.
+     */
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
