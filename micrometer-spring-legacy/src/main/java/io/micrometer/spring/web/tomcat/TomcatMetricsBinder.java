ac1baa68b8952fc6129c504d6f4a399592e69006
Oleksii Bondar <alexBondar.kha@gmail.com>
2019-01-03 14:27:02 -0800

    Address potential NPE in TomcatMetricsBinder when Context is null

--- micrometer-spring-legacy/src/main/java/io/micrometer/spring/web/tomcat/TomcatMetricsBinder.java
+++ micrometer-spring-legacy/src/main/java/io/micrometer/spring/web/tomcat/TomcatMetricsBinder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Pivotal Software, Inc.
+ * Copyright 2019 Pivotal Software, Inc.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,9 @@ public class TomcatMetricsBinder implements ApplicationListener<ApplicationReady
             EmbeddedServletContainer container = ((EmbeddedWebApplicationContext) applicationContext).getEmbeddedServletContainer();
             if (container instanceof TomcatEmbeddedServletContainer) {
                 Context context = findContext((TomcatEmbeddedServletContainer) container);
-                return context.getManager();
+                if (context != null) {
+                    return context.getManager();
+                }
             }
         }
         return null;
