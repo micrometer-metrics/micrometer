741ef34a47e1805a860f62d76502fd01d45355ed
Fabian Koehler <fab@fabiankoehler.de>
2019-01-08 19:45:56 +0100

    Do not write Infinity double values to elastic meter registry
    This resolves #1134

--- implementations/micrometer-registry-elastic/src/main/java/io/micrometer/elastic/ElasticMeterRegistry.java
+++ implementations/micrometer-registry-elastic/src/main/java/io/micrometer/elastic/ElasticMeterRegistry.java
@@ -184,7 +184,7 @@ public class ElasticMeterRegistry extends StepMeterRegistry {
     // VisibleForTesting
     Optional<String> writeGauge(Gauge gauge) {
         Double value = gauge.value();
-        if (!value.isNaN()) {
+        if (Double.isFinite(value)) {
             return Optional.of(writeDocument(gauge, builder -> {
                 builder.append(",\"value\":").append(value);
             }));
@@ -195,7 +195,7 @@ public class ElasticMeterRegistry extends StepMeterRegistry {
     // VisibleForTesting
     Optional<String> writeTimeGauge(TimeGauge gauge) {
         Double value = gauge.value();
-        if (!value.isNaN()) {
+        if (Double.isFinite(value)) {
             return Optional.of(writeDocument(gauge, builder -> {
                 builder.append(",\"value\":").append(gauge.value(getBaseTimeUnit()));
             }));
