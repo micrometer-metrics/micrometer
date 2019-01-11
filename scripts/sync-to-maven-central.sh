95ee1b7c2ad09519c51843cb188929fae92c8a70
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2019-01-11 19:39:51 +0900

    Fix maven central sync script
    Fixes two module names and uses the `module` variable in the URL called. Also checks that the version env var is set before doing anything, giving a helpful message.

--- scripts/sync-to-maven-central.sh
+++ scripts/sync-to-maven-central.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -z "$VERSION" ]]
+then
+    echo "\$VERSION must be set."
+    exit -1
+else
+    echo "Syncing version $VERSION"
+fi
+
 MODULES=(
     micrometer-core
     micrometer-test
@@ -16,14 +24,14 @@ MODULES=(
     micrometer-registry-signalfx
     micrometer-registry-statsd
     micrometer-registry-wavefront
-    micrometer-registry-spring-legacy
+    micrometer-spring-legacy
     micrometer-jersey2
     micrometer-registry-stackdriver
     micrometer-registry-elastic
     micrometer-registry-kairos
     micrometer-registry-dynatrace
     micrometer-registry-humio
-    micrometer-registry-azuremonitor
+    micrometer-registry-azure-monitor
     micrometer-registry-appoptics
 )
 
@@ -39,6 +47,6 @@ do
 			-H "Content-Type: application/json" -d "{\"username\": \"${SONATYPE_USER_TOKEN}\", \"password\": \"${SONATYPE_PASSWORD_TOKEN}\"}" \
 			-f \
 			-X \
-			POST "https://api.bintray.com/maven_central_sync/spring/jars/micrometer-registry-statsd/versions/${VERSION}" > /dev/null || { echo "Failed to sync" >&2; exit 1; }
+			POST "https://api.bintray.com/maven_central_sync/spring/jars/${module}/versions/${VERSION}" > /dev/null || { echo "Failed to sync" >&2; exit 1; }
     echo "  complete"
 done
