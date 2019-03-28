68af7ad2c97e431fa0ff7e42520835a5c0feceb9
Han Zhang <zhanghan@vmware.com>
2019-03-27 17:09:00 -0700

    Revert Wavefront base time unit

--- implementations/micrometer-registry-wavefront/src/main/java/io/micrometer/wavefront/WavefrontMeterRegistry.java
+++ implementations/micrometer-registry-wavefront/src/main/java/io/micrometer/wavefront/WavefrontMeterRegistry.java
@@ -401,7 +401,7 @@ public class WavefrontMeterRegistry extends PushMeterRegistry {
 
     @Override
     protected TimeUnit getBaseTimeUnit() {
-        return TimeUnit.MILLISECONDS;
+        return TimeUnit.SECONDS;
     }
 
     @Override
