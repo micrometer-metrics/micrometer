73ba459d567e724a676e86e78390e6fec36d70fa
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2019-04-11 21:59:22 +0900

    JavaDoc polish

--- micrometer-core/src/main/java/io/micrometer/core/instrument/distribution/HistogramGauges.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/distribution/HistogramGauges.java
@@ -37,8 +37,8 @@ public class HistogramGauges {
      * Register a set of gauges for percentiles and histogram buckets that follow a common format when
      * the monitoring system doesn't have an opinion about the structure of this data.
      *
-     * @param timer the timer to register
-     * @param registry the registry to register the timer
+     * @param timer the timer from which to derive gauges
+     * @param registry the registry to register the gauges
      * @return registered {@code HistogramGauges}
      */
     public static HistogramGauges registerWithCommonFormat(Timer timer, MeterRegistry registry) {
