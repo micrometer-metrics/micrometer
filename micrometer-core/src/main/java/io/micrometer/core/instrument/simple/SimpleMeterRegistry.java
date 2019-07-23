017f8ba8e5e2dd5ab3dd8ebd1d284bffb2b01eb4
Artur Burtsev <artjock@gmail.com>
2019-07-23 07:35:03 +0200

    Human-readable SimpleMeterRegistry toString
    Example:
    
    Timer response.time [tag(action=create)]
            count = 300
            total = 44.850000
             mean = 0.149500
              max = 0.299000
        percentileValues:
            75.0 <= 0.226459648
            90.0 <= 0.285179904
            95.0 <= 0.285179904
            99.0 <= 0.30195712
            99.9 <= 0.30195712
        histogramCounts:
            0.02 <= 21.0
            0.05 <= 51.0
             0.1 <= 101.0

--- micrometer-core/src/main/java/io/micrometer/core/instrument/simple/SimpleMeterRegistry.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/simple/SimpleMeterRegistry.java
@@ -16,9 +16,14 @@
 package io.micrometer.core.instrument.simple;
 
 import io.micrometer.core.instrument.*;
+import io.micrometer.core.instrument.Meter.Id;
 import io.micrometer.core.instrument.cumulative.*;
+import io.micrometer.core.instrument.distribution.CountAtBucket;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.core.instrument.distribution.HistogramGauges;
+import io.micrometer.core.instrument.distribution.HistogramSnapshot;
+import io.micrometer.core.instrument.distribution.HistogramSupport;
+import io.micrometer.core.instrument.distribution.ValueAtPercentile;
 import io.micrometer.core.instrument.distribution.pause.PauseDetector;
 import io.micrometer.core.instrument.internal.DefaultGauge;
 import io.micrometer.core.instrument.internal.DefaultLongTaskTimer;
@@ -26,9 +31,13 @@ import io.micrometer.core.instrument.internal.DefaultMeter;
 import io.micrometer.core.instrument.step.*;
 import io.micrometer.core.lang.Nullable;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.ToDoubleFunction;
 import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
 
 /**
  * A minimal meter registry implementation primarily used for tests.
@@ -153,5 +162,141 @@ public class SimpleMeterRegistry extends MeterRegistry {
                 .expiry(config.step())
                 .build()
                 .merge(DistributionStatisticConfig.DEFAULT);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        getMeters().stream()
+                .sorted(Comparator.comparing(m -> m.getId().getName()))
+                .forEach(m -> m.use(
+                        gauge -> appendMeter(gauge, builder),
+                        counter -> appendMeter(counter, builder),
+                        timer -> appendMeter(timer, builder),
+                        summary -> appendMeter(summary, builder),
+                        longTaskTimer -> appendMeter(longTaskTimer, builder),
+                        timeGauge -> appendMeter(timeGauge, builder),
+                        functionCounter -> appendMeter(functionCounter, builder),
+                        functionTimer -> appendMeter(functionTimer, builder),
+                        meter -> appendMeter(meter, builder)
+                ));
+
+        return builder.toString();
+    }
+
+    private String getMeterName(Meter meter, String type) {
+
+        final Id meterId = meter.getId();
+        final List<Tag> tags = meterId.getTags();
+
+        String meterName = "\n" + type + " " + meterId.getName();
+
+        if (!tags.isEmpty()) {
+            meterName += " {" + tags.stream()
+                    .map(t -> t.getKey() + "=" + t.getValue())
+                    .collect(Collectors.joining("; "))
+                    + "}";
+        }
+
+        return meterName;
+    }
+
+    private String getMeterValueFormat(String separator, String conversion) {
+        final int padding = 16 - separator.length();
+        return "\n%1$" + padding + "s" + separator + "%2$" + conversion;
+    }
+
+    private void appendMeter(Gauge gauge, StringBuilder builder) {
+        builder.append(String.format("%s value=%f", getMeterName(gauge, "Gauge"), gauge.value()));
+    }
+
+    private void appendMeter(Counter counter, StringBuilder builder) {
+        builder.append(String.format("%s count=%f", getMeterName(counter, "Counter"), counter.count()));
+    }
+
+    private void appendMeter(Timer timer, StringBuilder builder) {
+        appendHistogram(timer, "Timer", builder, getBaseTimeUnit());
+    }
+
+    private void appendMeter(DistributionSummary summary, StringBuilder builder) {
+        appendHistogram(summary, "DistributionSummary", builder, null);
+    }
+
+    private void appendHistogram(HistogramSupport summary, String type, StringBuilder builder,
+            @Nullable TimeUnit timeUnit) {
+        final HistogramSnapshot snapshot = summary.takeSnapshot();
+
+        final double total = timeUnit == null ? snapshot.total() : snapshot.total(timeUnit);
+        final double mean = timeUnit == null ? snapshot.mean() : snapshot.mean(timeUnit);
+        final double max = timeUnit == null ? snapshot.max() : snapshot.max(timeUnit);
+
+        builder
+                .append(getMeterName(summary, type))
+                .append(String.format(getMeterValueFormat(" = ", "d"), "count", snapshot.count()))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "total", total))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "mean", mean))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "max", max))
+        ;
+
+        final ValueAtPercentile[] percentileValues = snapshot.percentileValues();
+        if (percentileValues.length > 0) {
+            builder.append("\n    percentileValues:");
+            Arrays.stream(percentileValues)
+                    .sorted(Comparator.comparingDouble(ValueAtPercentile::percentile))
+                    .forEach(vap -> builder.append(String.format(
+                            getMeterValueFormat(" <= ", "s"),
+                            vap.percentile() * 100,
+                            timeUnit == null ? vap.value() : vap.value(getBaseTimeUnit())
+                    )));
+        }
+
+        final CountAtBucket[] histogramCounts = snapshot.histogramCounts();
+        if (histogramCounts.length > 0) {
+            builder.append("\n    histogramCounts:");
+            Arrays.stream(histogramCounts)
+                    .sorted(Comparator.comparingDouble(CountAtBucket::bucket))
+                    .forEach(hc -> builder.append(String.format(
+                            getMeterValueFormat(" <= ", "s"),
+                            timeUnit == null ? hc.bucket() : hc.bucket(getBaseTimeUnit()),
+                            hc.count()
+                    )));
+        }
+    }
+
+    private void appendMeter(LongTaskTimer timer, StringBuilder builder) {
+        builder
+                .append(getMeterName(timer, "Timer"))
+                .append(String.format(getMeterValueFormat(" = ", "d"), "activeTasks", timer.activeTasks()))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "duration",
+                        timer.duration(getBaseTimeUnit())));
+    }
+
+    private void appendMeter(TimeGauge gauge, StringBuilder builder) {
+        builder.append(String.format("%s value=%f", getMeterName(gauge, "Gauge"),
+                gauge.value(getBaseTimeUnit())));
+    }
+
+
+    private void appendMeter(FunctionCounter counter, StringBuilder builder) {
+        builder.append(String.format("%s count=%f", getMeterName(counter, "Counter"), counter.count()));
+    }
+
+    private void appendMeter(FunctionTimer timer, StringBuilder builder) {
+        builder
+                .append(getMeterName(timer, "Timer"))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "count", timer.count()))
+                .append(String.format(getMeterValueFormat(" = ", "f"),
+                        "totalTime", timer.totalTime(getBaseTimeUnit())))
+                .append(String.format(getMeterValueFormat(" = ", "f"), "mean",
+                        timer.mean(getBaseTimeUnit())));
+    }
+
+    private void appendMeter(Meter meter, StringBuilder builder) {
+        builder.append(getMeterName(meter, "Gauge"));
+
+        meter.measure().forEach(measurement -> builder.append(
+                String.format(getMeterValueFormat(" = ", "f"),
+                        measurement.getStatistic(), measurement.getValue())));
     }
 }
--- micrometer-core/src/test/java/io/micrometer/core/instrument/simple/SimpleMeterRegistryTest.java
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/simple/SimpleMeterRegistryTest.java
@@ -16,18 +16,26 @@
 package io.micrometer.core.instrument.simple;
 
 import io.micrometer.core.Issue;
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.FunctionCounter;
 import io.micrometer.core.instrument.FunctionTimer;
 import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.LongTaskTimer;
+import io.micrometer.core.instrument.Measurement;
 import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Meter.Type;
 import io.micrometer.core.instrument.MockClock;
+import io.micrometer.core.instrument.Statistic;
 import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.TimeGauge;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.cumulative.CumulativeFunctionCounter;
 import io.micrometer.core.instrument.cumulative.CumulativeFunctionTimer;
 import io.micrometer.core.instrument.step.StepFunctionCounter;
 import io.micrometer.core.instrument.step.StepFunctionTimer;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -99,6 +107,133 @@ class SimpleMeterRegistryTest {
         Meter.Id id = new Meter.Id("some.timer", Tags.empty(), null, null, Meter.Type.COUNTER);
         FunctionCounter functionCounter = registry.newFunctionCounter(id, null, (o) -> 0d);
         assertThat(functionCounter).isInstanceOf(StepFunctionCounter.class);
+    }
+
+    @Test
+    public void toStringOutputTest() {
+        final SimpleMeterRegistry registry = new SimpleMeterRegistry();
+
+        final Counter requestSizeCounter = Counter.builder("request.size")
+                .baseUnit("bytes")
+                .register(registry);
+
+        final DistributionSummary responseSizeSummary = DistributionSummary.builder("response.size")
+                .baseUnit("bytes")
+                .publishPercentileHistogram()
+                .publishPercentiles(0.9999, 0.99, 0.95, 0.90, 0.75)
+                .sla(250, 500, 1000, 2000, 4000, 6000)
+                .register(registry);
+
+        final Timer responseTimer = Timer.builder("response.time")
+                .tag("action", "create")
+                .publishPercentiles(0.999, 0.99, 0.95, 0.90, 0.75)
+                .sla(Duration.ofMillis(20), Duration.ofMillis(50), Duration.ofMillis(100))
+                .publishPercentileHistogram()
+                .register(registry);
+
+        final AtomicLong testRunTime = new AtomicLong();
+        TimeGauge
+                .builder("test.run.time", testRunTime, TimeUnit.NANOSECONDS, AtomicLong::doubleValue)
+                .register(registry);
+
+        FunctionTimer
+                .builder("cache.latency", new Object(), o -> 10, o -> 300, TimeUnit.MILLISECONDS)
+                .register(registry);
+
+        FunctionCounter
+                .builder("cache.evictions", new Object(), o -> 10)
+                .baseUnit("beans")
+                .register(registry);
+
+        final LongTaskTimer taskTimer = LongTaskTimer
+                .builder("test.loop.timer")
+                .register(registry);
+
+        Meter.builder("default.meter", Type.GAUGE,
+                Arrays.asList(
+                        new Measurement(() -> 7.7, Statistic.VALUE),
+                        new Measurement(() -> 8.8, Statistic.VALUE)
+                ))
+                .baseUnit("beans")
+                .register(registry);
+
+        final LongTaskTimer.Sample loopSample = taskTimer.start();
+
+        for (int i = 0; i < 300; i++) {
+            responseTimer.record(Duration.ofMillis(i));
+            requestSizeCounter.increment(i);
+            responseSizeSummary.record(i);
+        }
+
+        loopSample.stop();
+
+        testRunTime.set(200);
+        assertThat(registry.toString()).isEqualTo("\n"
+                + "Counter cache.evictions count=10.000000\n"
+                + "Timer cache.latency\n"
+                + "        count = 10.000000\n"
+                + "    totalTime = 0.300000\n"
+                + "         mean = 0.030000\n"
+                + "Gauge default.meter\n"
+                + "        VALUE = 7.700000\n"
+                + "        VALUE = 8.800000\n"
+                + "Counter request.size count=44850.000000\n"
+                + "DistributionSummary response.size\n"
+                + "        count = 300\n"
+                + "        total = 44850.000000\n"
+                + "         mean = 149.500000\n"
+                + "          max = 299.000000\n"
+                + "    percentileValues:\n"
+                + "        75.0 <= 231.9375\n"
+                + "        90.0 <= 271.9375\n"
+                + "        95.0 <= 287.9375\n"
+                + "        99.0 <= 303.9375\n"
+                + "       99.99 <= 303.9375\n"
+                + "    histogramCounts:\n"
+                + "       250.0 <= 256.0\n"
+                + "       500.0 <= 300.0\n"
+                + "      1000.0 <= 300.0\n"
+                + "      2000.0 <= 300.0\n"
+                + "      4000.0 <= 300.0\n"
+                + "      6000.0 <= 300.0\n"
+                + "Gauge response.size.histogram {le=250} value=256.000000\n"
+                + "Gauge response.size.histogram {le=500} value=300.000000\n"
+                + "Gauge response.size.histogram {le=1000} value=300.000000\n"
+                + "Gauge response.size.histogram {le=2000} value=300.000000\n"
+                + "Gauge response.size.histogram {le=4000} value=300.000000\n"
+                + "Gauge response.size.histogram {le=6000} value=300.000000\n"
+                + "Gauge response.size.percentile {phi=0.9} value=271.937500\n"
+                + "Gauge response.size.percentile {phi=0.75} value=231.937500\n"
+                + "Gauge response.size.percentile {phi=0.95} value=287.937500\n"
+                + "Gauge response.size.percentile {phi=0.99} value=303.937500\n"
+                + "Gauge response.size.percentile {phi=0.9999} value=303.937500\n"
+                + "Timer response.time {action=create}\n"
+                + "        count = 300\n"
+                + "        total = 44.850000\n"
+                + "         mean = 0.149500\n"
+                + "          max = 0.299000\n"
+                + "    percentileValues:\n"
+                + "        75.0 <= 0.226459648\n"
+                + "        90.0 <= 0.285179904\n"
+                + "        95.0 <= 0.285179904\n"
+                + "        99.0 <= 0.30195712\n"
+                + "        99.9 <= 0.30195712\n"
+                + "    histogramCounts:\n"
+                + "        0.02 <= 21.0\n"
+                + "        0.05 <= 51.0\n"
+                + "         0.1 <= 101.0\n"
+                + "Gauge response.time.histogram {action=create; le=0.1} value=101.000000\n"
+                + "Gauge response.time.histogram {action=create; le=0.02} value=21.000000\n"
+                + "Gauge response.time.histogram {action=create; le=0.05} value=51.000000\n"
+                + "Gauge response.time.percentile {action=create; phi=0.9} value=0.285180\n"
+                + "Gauge response.time.percentile {action=create; phi=0.75} value=0.226460\n"
+                + "Gauge response.time.percentile {action=create; phi=0.95} value=0.285180\n"
+                + "Gauge response.time.percentile {action=create; phi=0.99} value=0.301957\n"
+                + "Gauge response.time.percentile {action=create; phi=0.999} value=0.301957\n"
+                + "Timer test.loop.timer\n"
+                + "  activeTasks = 0\n"
+                + "     duration = 0.000000\n"
+                + "Gauge test.run.time value=0.000000");
     }
 
     private SimpleMeterRegistry createRegistry(CountingMode mode) {
