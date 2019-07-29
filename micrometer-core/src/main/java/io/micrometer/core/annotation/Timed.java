998107c9d09431fad657533d1666400b98d98d3f
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2019-07-29 19:08:08 +0900

    JavaDocs for Timed annotation methods
    Resolves #1535

--- micrometer-core/src/main/java/io/micrometer/core/annotation/Timed.java
+++ micrometer-core/src/main/java/io/micrometer/core/annotation/Timed.java
@@ -30,16 +30,42 @@ public @interface Timed {
     String value() default "";
 
     /**
-     * A list of key-value pair arguments to supply the underlying Timer 
-     * {@link io.micrometer.core.instrument.Timer.Builder#tags(String...)} 
+     * List of key-value pair arguments to supply the Timer as extra tags.
+     *
+     * @return key-value pair of tags
+     * @see io.micrometer.core.instrument.Timer.Builder#tags(String...)
      */
     String[] extraTags() default {};
 
+    /**
+     * Flag of whether the Timer should be a {@link io.micrometer.core.instrument.LongTaskTimer}.
+     *
+     * @return whether the timer is a LongTaskTimer
+     */
     boolean longTask() default false;
 
+    /**
+     * List of percentiles to calculate client-side for the {@link io.micrometer.core.instrument.Timer}.
+     * For example, the 95th percentile should be passed as {@code 0.95}.
+     * 
+     * @return percentiles to calculate
+     * @see io.micrometer.core.instrument.Timer.Builder#publishPercentiles(double...) 
+     */
     double[] percentiles() default {};
 
+    /**
+     * Whether to enable recording of a percentile histogram for the {@link io.micrometer.core.instrument.Timer Timer}.
+     * 
+     * @return whether percentile histogram is enabled
+     * @see io.micrometer.core.instrument.Timer.Builder#publishPercentileHistogram(Boolean) 
+     */
     boolean histogram() default false;
 
+    /**
+     * Description of the {@link io.micrometer.core.instrument.Timer}.
+     *
+     * @return meter description
+     * @see io.micrometer.core.instrument.Timer.Builder#description(String)
+     */
     String description() default "";
 }
