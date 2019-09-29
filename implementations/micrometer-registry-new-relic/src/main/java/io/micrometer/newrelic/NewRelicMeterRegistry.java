56f275b0b6e47a96e2f6d69d40d3dc8cf6ed3d8e
Neil Powell <52715665+neiljpowell@users.noreply.github.com>
2019-09-29 12:57:16 -0500

    Add support for categorical "MicrometerSample" eventType to publish New Relic metrics. (#1588)
    Previous behavior was to publish an eventType per Meter/metric name
    (still supported via meterNameEventTypeEnabled=true). NewRelic's own
    agents publish metrics to eventTypes aligned with broader categories. To
    align with their recommendation the default behavior is to publish
    metrics under a "MicrometerSample" category. When doing so, additional
    context is provided by including "metricName" and "metricType"
    attributes. Also "timeUnit" is now provided as an attribute for all
    Timer related metrics regardless, so Insights users can better
    understand time-based metrics.
    See: https://docs.newrelic.com/docs/insights/insights-data-sources/default-data/insights-default-data-other-new-relic-products

--- implementations/micrometer-registry-new-relic/src/main/java/io/micrometer/newrelic/NewRelicMeterRegistry.java
+++ implementations/micrometer-registry-new-relic/src/main/java/io/micrometer/newrelic/NewRelicMeterRegistry.java
@@ -15,33 +15,7 @@
  */
 package io.micrometer.newrelic;
 
-import static io.micrometer.core.instrument.util.StringEscapeUtils.escapeJson;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.DistributionSummary;
-import io.micrometer.core.instrument.FunctionCounter;
-import io.micrometer.core.instrument.FunctionTimer;
-import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.LongTaskTimer;
-import io.micrometer.core.instrument.Measurement;
-import io.micrometer.core.instrument.Meter;
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Tags;
-import io.micrometer.core.instrument.TimeGauge;
+import io.micrometer.core.instrument.*;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.config.MissingRequiredConfigurationException;
 import io.micrometer.core.instrument.config.NamingConvention;
@@ -52,6 +26,18 @@ import io.micrometer.core.instrument.util.NamedThreadFactory;
 import io.micrometer.core.instrument.util.TimeUtils;
 import io.micrometer.core.ipc.http.HttpSender;
 import io.micrometer.core.ipc.http.HttpUrlConnectionSender;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static io.micrometer.core.instrument.util.StringEscapeUtils.escapeJson;
 
 /**
  * Publishes metrics to New Relic Insights.
@@ -87,14 +73,22 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
         this(config, clock, threadFactory, new HttpUrlConnectionSender(config.connectTimeout(), config.readTimeout()));
     }
 
-    private NewRelicMeterRegistry(NewRelicConfig config, Clock clock, ThreadFactory threadFactory, HttpSender httpClient) {
+    // VisibleForTesting
+    NewRelicMeterRegistry(NewRelicConfig config, Clock clock, ThreadFactory threadFactory, HttpSender httpClient) {
         super(config, clock);
 
-        if (config.accountId() == null) {
+        if (config.meterNameEventTypeEnabled() == false
+                && (config.eventType() == null || config.eventType().isEmpty())) {
+            throw new MissingRequiredConfigurationException("eventType must be set to report metrics to New Relic");
+        }
+        if (config.accountId() == null || config.accountId().isEmpty()) {
             throw new MissingRequiredConfigurationException("accountId must be set to report metrics to New Relic");
         }
-        if (config.apiKey() == null) {
+        if (config.apiKey() == null || config.apiKey().isEmpty()) {
             throw new MissingRequiredConfigurationException("apiKey must be set to report metrics to New Relic");
+        }
+        if (config.uri() == null || config.uri().isEmpty()) {
+            throw new MissingRequiredConfigurationException("uri must be set to report metrics to New Relic");
         }
 
         this.config = config;
@@ -140,7 +134,8 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
         return Stream.of(
                 event(ltt.getId(),
                         new Attribute("activeTasks", ltt.activeTasks()),
-                        new Attribute("duration", ltt.duration(getBaseTimeUnit())))
+                        new Attribute("duration", ltt.duration(getBaseTimeUnit())),
+                        new Attribute("timeUnit", getBaseTimeUnit().name().toLowerCase()))
         );
     }
 
@@ -154,6 +149,7 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
     }
 
     private Stream<String> writeCounter(Counter counter) {
+    	//TODO: Double.isFinite() check here like writeFunctionCounter ???
         return Stream.of(event(counter.getId(), new Attribute("throughput", counter.count())));
     }
 
@@ -170,7 +166,10 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
     Stream<String> writeTimeGauge(TimeGauge gauge) {
         Double value = gauge.value(getBaseTimeUnit());
         if (Double.isFinite(value)) {
-            return Stream.of(event(gauge.getId(), new Attribute("value", value)));
+            return Stream.of(
+                    event(gauge.getId(), 
+                            new Attribute("value", value),
+                            new Attribute("timeUnit", getBaseTimeUnit().name().toLowerCase())));
         }
         return Stream.empty();
     }
@@ -191,7 +190,8 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
                 new Attribute("count", timer.count()),
                 new Attribute("avg", timer.mean(getBaseTimeUnit())),
                 new Attribute("totalTime", timer.totalTime(getBaseTimeUnit())),
-                new Attribute("max", timer.max(getBaseTimeUnit()))
+                new Attribute("max", timer.max(getBaseTimeUnit())),
+                new Attribute("timeUnit", getBaseTimeUnit().name().toLowerCase())
         ));
     }
 
@@ -200,7 +200,8 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
                 event(timer.getId(),
                         new Attribute("count", timer.count()),
                         new Attribute("avg", timer.mean(getBaseTimeUnit())),
-                        new Attribute("totalTime", timer.totalTime(getBaseTimeUnit()))
+                        new Attribute("totalTime", timer.totalTime(getBaseTimeUnit())),
+                        new Attribute("timeUnit", getBaseTimeUnit().name().toLowerCase())
                 )
         );
     }
@@ -224,6 +225,18 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
     }
 
     private String event(Meter.Id id, Attribute... attributes) {
+        if (config.meterNameEventTypeEnabled() == false) {
+            //Include contextual attributes when publishing all metrics under a single categorical eventType,
+            //  NOT when publishing an eventType per Meter/metric name
+            int size = attributes.length;
+            Attribute[] newAttrs = Arrays.copyOf(attributes, size + 2);
+            
+            String name = id.getConventionName(config().namingConvention());
+            newAttrs[size] = new Attribute("metricName", name);
+            newAttrs[size + 1] = new Attribute("metricType", id.getType().toString());
+            
+            return event(id, Tags.empty(), newAttrs);
+        }
         return event(id, Tags.empty(), attributes);
     }
 
@@ -240,11 +253,29 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
                     .append("\":\"").append(escapeJson(convention.tagValue(tag.getValue()))).append("\"");
         }
 
+        String eventType = getEventType(id, config, convention);
+        
         return Arrays.stream(attributes)
-                .map(attr -> ",\"" + attr.getName() + "\":" + DoubleFormat.wholeOrDecimal(attr.getValue().doubleValue()))
-                .collect(Collectors.joining("", "{\"eventType\":\"" + escapeJson(getConventionName(id)) + "\"", tagsJson + "}"));
+                .map(attr ->
+                        (attr.getValue() instanceof Number)
+                            ? ",\"" + attr.getName() + "\":" + DoubleFormat.wholeOrDecimal(((Number)attr.getValue()).doubleValue())
+                            : ",\"" + attr.getName() + "\":\"" + convention.tagValue(attr.getValue().toString()) + "\""
+                )
+                .collect(Collectors.joining("", "{\"eventType\":\"" + escapeJson(eventType) + "\"", tagsJson + "}"));
     }
 
+    String getEventType(Meter.Id id, NewRelicConfig config, NamingConvention convention) {
+        String eventType = null;
+        if (config.meterNameEventTypeEnabled()) {
+            //meter/metric name event type
+            eventType = id.getConventionName(convention);
+        } else {
+            //static eventType "category"
+            eventType = config.eventType();
+        }
+        return eventType;
+    }
+    
     private void sendEvents(String insightsEndpoint, Stream<String> events) {
         try {
             AtomicInteger totalEvents = new AtomicInteger();
@@ -300,9 +331,9 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
 
     private class Attribute {
         private final String name;
-        private final Number value;
+        private final Object value;
 
-        private Attribute(String name, Number value) {
+        private Attribute(String name, Object value) {
             this.name = name;
             this.value = value;
         }
@@ -311,7 +342,7 @@ public class NewRelicMeterRegistry extends StepMeterRegistry {
             return name;
         }
 
-        public Number getValue() {
+        public Object getValue() {
             return value;
         }
     }
--- implementations/micrometer-registry-new-relic/src/test/java/io/micrometer/newrelic/NewRelicMeterRegistryTest.java
+++ implementations/micrometer-registry-new-relic/src/test/java/io/micrometer/newrelic/NewRelicMeterRegistryTest.java
@@ -15,10 +15,16 @@
  */
 package io.micrometer.newrelic;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
 
 import io.micrometer.core.instrument.FunctionCounter;
 import io.micrometer.core.instrument.Gauge;
@@ -28,9 +34,9 @@ import io.micrometer.core.instrument.MockClock;
 import io.micrometer.core.instrument.Statistic;
 import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.TimeGauge;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import io.micrometer.core.instrument.config.MissingRequiredConfigurationException;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import io.micrometer.core.ipc.http.HttpSender;
 
 /**
  * Tests for {@link NewRelicMeterRegistry}.
@@ -40,46 +46,97 @@ import static org.assertj.core.api.Assertions.assertThat;
 class NewRelicMeterRegistryTest {
 
     private final NewRelicConfig config = new NewRelicConfig() {
-
+        
+        @Override
+        public boolean meterNameEventTypeEnabled() {
+            //Default is false. Publish all metrics under a single eventType
+            return NewRelicConfig.super.meterNameEventTypeEnabled();
+        }
+        
         @Override
         public String get(String key) {
             return null;
         }
-
+        
         @Override
         public String accountId() {
             return "accountId";
         }
-
+        
         @Override
         public String apiKey() {
             return "apiKey";
         }
-
+        
     };
+   
+    private final NewRelicConfig meterNameEventTypeEnabledConfig = new NewRelicConfig() {
+        
+        @Override
+        public boolean meterNameEventTypeEnabled() {
+            //Previous behavior for backward compatibility
+            return true;
+        }
+        
+        @Override
+        public String get(String key) {
+            return null;
+        }
+        
+        @Override
+        public String accountId() {
+            return "accountId";
+        }
+        
+        @Override
+        public String apiKey() {
+            return "apiKey";
+        }
+        
+    };
+    
     private final MockClock clock = new MockClock();
+    private final NewRelicMeterRegistry meterNameEventTypeEnabledRegistry = new NewRelicMeterRegistry(meterNameEventTypeEnabledConfig, clock);
     private final NewRelicMeterRegistry registry = new NewRelicMeterRegistry(config, clock);
 
     @Test
     void writeGauge() {
+        meterNameEventTypeEnabledRegistry.gauge("my.gauge", 1d);
+        Gauge gauge = meterNameEventTypeEnabledRegistry.find("my.gauge").gauge(); 
+        Stream<String> streamResult = meterNameEventTypeEnabledRegistry.writeGauge(gauge);
+        assertThat(streamResult).contains("{\"eventType\":\"myGauge\",\"value\":1}");
+        
         registry.gauge("my.gauge", 1d);
-        Gauge gauge = registry.find("my.gauge").gauge();
-        assertThat(registry.writeGauge(gauge)).hasSize(1);
+        gauge = registry.find("my.gauge").gauge(); 
+        streamResult = registry.writeGauge(gauge);
+        assertThat(streamResult).contains("{\"eventType\":\"MicrometerSample\",\"value\":1,\"metricName\":\"myGauge\",\"metricType\":\"GAUGE\"}");
     }
 
     @Test
     void writeGaugeShouldDropNanValue() {
+        meterNameEventTypeEnabledRegistry.gauge("my.gauge", Double.NaN);
+        Gauge gauge = meterNameEventTypeEnabledRegistry.find("my.gauge").gauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeGauge(gauge)).isEmpty();
+        
         registry.gauge("my.gauge", Double.NaN);
-        Gauge gauge = registry.find("my.gauge").gauge();
+        gauge = registry.find("my.gauge").gauge();
         assertThat(registry.writeGauge(gauge)).isEmpty();
     }
 
     @Test
     void writeGaugeShouldDropInfiniteValues() {
+        meterNameEventTypeEnabledRegistry.gauge("my.gauge", Double.POSITIVE_INFINITY);
+        Gauge gauge = meterNameEventTypeEnabledRegistry.find("my.gauge").gauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeGauge(gauge)).isEmpty();
+        
+        meterNameEventTypeEnabledRegistry.gauge("my.gauge", Double.NEGATIVE_INFINITY);
+        gauge = meterNameEventTypeEnabledRegistry.find("my.gauge").gauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeGauge(gauge)).isEmpty();
+        
         registry.gauge("my.gauge", Double.POSITIVE_INFINITY);
-        Gauge gauge = registry.find("my.gauge").gauge();
+        gauge = registry.find("my.gauge").gauge();
         assertThat(registry.writeGauge(gauge)).isEmpty();
-
+        
         registry.gauge("my.gauge", Double.NEGATIVE_INFINITY);
         gauge = registry.find("my.gauge").gauge();
         assertThat(registry.writeGauge(gauge)).isEmpty();
@@ -88,26 +145,46 @@ class NewRelicMeterRegistryTest {
     @Test
     void writeGaugeWithTimeGauge() {
         AtomicReference<Double> obj = new AtomicReference<>(1d);
+        meterNameEventTypeEnabledRegistry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
+        TimeGauge timeGauge = meterNameEventTypeEnabledRegistry.find("my.timeGauge").timeGauge();
+        Stream<String> streamResult = meterNameEventTypeEnabledRegistry.writeTimeGauge(timeGauge);
+        assertThat(streamResult).contains("{\"eventType\":\"myTimeGauge\",\"value\":1,\"timeUnit\":\"seconds\"}");
+        
         registry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
-        TimeGauge timeGauge = registry.find("my.timeGauge").timeGauge();
-        assertThat(registry.writeTimeGauge(timeGauge)).hasSize(1);
+        timeGauge = registry.find("my.timeGauge").timeGauge();
+        streamResult = registry.writeTimeGauge(timeGauge);
+        assertThat(streamResult).contains("{\"eventType\":\"MicrometerSample\",\"value\":1,\"timeUnit\":\"seconds\",\"metricName\":\"myTimeGauge\",\"metricType\":\"GAUGE\"}");
     }
 
     @Test
     void writeGaugeWithTimeGaugeShouldDropNanValue() {
         AtomicReference<Double> obj = new AtomicReference<>(Double.NaN);
+        meterNameEventTypeEnabledRegistry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
+        TimeGauge timeGauge = meterNameEventTypeEnabledRegistry.find("my.timeGauge").timeGauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeTimeGauge(timeGauge)).isEmpty();
+        
         registry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
-        TimeGauge timeGauge = registry.find("my.timeGauge").timeGauge();
+        timeGauge = registry.find("my.timeGauge").timeGauge();
         assertThat(registry.writeTimeGauge(timeGauge)).isEmpty();
     }
 
     @Test
     void writeGaugeWithTimeGaugeShouldDropInfiniteValues() {
         AtomicReference<Double> obj = new AtomicReference<>(Double.POSITIVE_INFINITY);
+        meterNameEventTypeEnabledRegistry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
+        TimeGauge timeGauge = meterNameEventTypeEnabledRegistry.find("my.timeGauge").timeGauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeTimeGauge(timeGauge)).isEmpty();
+        
+        obj = new AtomicReference<>(Double.NEGATIVE_INFINITY);
+        meterNameEventTypeEnabledRegistry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
+        timeGauge = meterNameEventTypeEnabledRegistry.find("my.timeGauge").timeGauge();
+        assertThat(meterNameEventTypeEnabledRegistry.writeTimeGauge(timeGauge)).isEmpty();
+        
+        obj = new AtomicReference<>(Double.POSITIVE_INFINITY);
         registry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
-        TimeGauge timeGauge = registry.find("my.timeGauge").timeGauge();
+        timeGauge = registry.find("my.timeGauge").timeGauge();
         assertThat(registry.writeTimeGauge(timeGauge)).isEmpty();
-
+        
         obj = new AtomicReference<>(Double.NEGATIVE_INFINITY);
         registry.more().timeGauge("my.timeGauge", Tags.empty(), obj, TimeUnit.SECONDS, AtomicReference::get);
         timeGauge = registry.find("my.timeGauge").timeGauge();
@@ -116,17 +193,31 @@ class NewRelicMeterRegistryTest {
 
     @Test
     void writeCounterWithFunctionCounter() {
-        FunctionCounter counter = FunctionCounter.builder("myCounter", 1d, Number::doubleValue).register(registry);
+        FunctionCounter counter = FunctionCounter.builder("myCounter", 1d, Number::doubleValue).register(meterNameEventTypeEnabledRegistry);
         clock.add(config.step());
-        assertThat(registry.writeFunctionCounter(counter)).hasSize(1);
+        Stream<String> streamResult = meterNameEventTypeEnabledRegistry.writeFunctionCounter(counter);
+        assertThat(streamResult).contains("{\"eventType\":\"myCounter\",\"throughput\":1}");
+        
+        counter = FunctionCounter.builder("myCounter", 1d, Number::doubleValue).register(registry);
+        clock.add(config.step());
+        streamResult = registry.writeFunctionCounter(counter);
+        assertThat(streamResult).contains("{\"eventType\":\"MicrometerSample\",\"throughput\":1,\"metricName\":\"myCounter\",\"metricType\":\"COUNTER\"}");
     }
 
     @Test
     void writeCounterWithFunctionCounterShouldDropInfiniteValues() {
-        FunctionCounter counter = FunctionCounter.builder("myCounter", Double.POSITIVE_INFINITY, Number::doubleValue).register(registry);
+        FunctionCounter counter = FunctionCounter.builder("myCounter", Double.POSITIVE_INFINITY, Number::doubleValue).register(meterNameEventTypeEnabledRegistry);
+        clock.add(config.step());
+        assertThat(meterNameEventTypeEnabledRegistry.writeFunctionCounter(counter)).isEmpty();
+        
+        counter = FunctionCounter.builder("myCounter", Double.NEGATIVE_INFINITY, Number::doubleValue).register(meterNameEventTypeEnabledRegistry);
+        clock.add(config.step());
+        assertThat(meterNameEventTypeEnabledRegistry.writeFunctionCounter(counter)).isEmpty();
+        
+        counter = FunctionCounter.builder("myCounter", Double.POSITIVE_INFINITY, Number::doubleValue).register(registry);
         clock.add(config.step());
         assertThat(registry.writeFunctionCounter(counter)).isEmpty();
-
+        
         counter = FunctionCounter.builder("myCounter", Double.NEGATIVE_INFINITY, Number::doubleValue).register(registry);
         clock.add(config.step());
         assertThat(registry.writeFunctionCounter(counter)).isEmpty();
@@ -138,7 +229,10 @@ class NewRelicMeterRegistryTest {
         Measurement measurement2 = new Measurement(() -> Double.NEGATIVE_INFINITY, Statistic.VALUE);
         Measurement measurement3 = new Measurement(() -> Double.NaN, Statistic.VALUE);
         List<Measurement> measurements = Arrays.asList(measurement1, measurement2, measurement3);
-        Meter meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.registry);
+        Meter meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.meterNameEventTypeEnabledRegistry);
+        assertThat(meterNameEventTypeEnabledRegistry.writeMeter(meter)).isEmpty();
+        
+        meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.registry);
         assertThat(registry.writeMeter(meter)).isEmpty();
     }
 
@@ -149,10 +243,13 @@ class NewRelicMeterRegistryTest {
         Measurement measurement3 = new Measurement(() -> Double.NaN, Statistic.VALUE);
         Measurement measurement4 = new Measurement(() -> 1d, Statistic.VALUE);
         List<Measurement> measurements = Arrays.asList(measurement1, measurement2, measurement3, measurement4);
-        Meter meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.registry);
-        assertThat(registry.writeMeter(meter)).contains("{\"eventType\":\"myMeter\",\"value\":1}");
+        Meter meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.meterNameEventTypeEnabledRegistry);
+        assertThat(meterNameEventTypeEnabledRegistry.writeMeter(meter)).contains("{\"eventType\":\"myMeter\",\"value\":1}");
+        
+        meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.registry);
+        assertThat(registry.writeMeter(meter)).contains("{\"eventType\":\"MicrometerSample\",\"value\":1,\"metricName\":\"myMeter\",\"metricType\":\"GAUGE\"}");
     }
-    
+  
     @Test
     void writeMeterWhenCustomMeterHasDuplicatesKeysShouldWriteOnlyLastValue() {
         Measurement measurement1 = new Measurement(() -> 3d, Statistic.VALUE);
@@ -160,8 +257,137 @@ class NewRelicMeterRegistryTest {
         Measurement measurement3 = new Measurement(() -> 2d, Statistic.VALUE);
         List<Measurement> measurements = Arrays.asList(measurement1, measurement2, measurement3);
         Meter meter = Meter.builder("my.meter", Meter.Type.GAUGE, measurements).register(this.registry);
-        assertThat(registry.writeMeter(meter)).contains("{\"eventType\":\"myMeter\",\"value\":2}");
+        assertThat(registry.writeMeter(meter)).contains("{\"eventType\":\"MicrometerSample\",\"value\":2,\"metricName\":\"myMeter\",\"metricType\":\"GAUGE\"}");
     }
 
+    @Test
+    void publish() {
+        MockHttpSender mockHttpSender = new MockHttpSender();
+        NewRelicMeterRegistry registry = new NewRelicMeterRegistry(config, clock, new NamedThreadFactory("new-relic-test"), mockHttpSender);
+        
+        registry.gauge("my.gauge", 1d);
+        Gauge gauge = registry.find("my.gauge").gauge();
+        assertThat(gauge).isNotNull();
+        
+        registry.publish();
+        
+        assertThat(new String(mockHttpSender.getRequest().getEntity()))
+            .contains("{\"eventType\":\"MicrometerSample\",\"value\":1,\"metricName\":\"myGauge\",\"metricType\":\"GAUGE\"}");
+    }
+
+    @Test
+    void configMissingEventType() {
+        NewRelicConfig config = new NewRelicConfig() {
+            @Override
+            public String eventType() {
+                return "";
+            }
+            @Override
+            public String get(String key) {
+                return null;
+            }
+        };
+        
+        Exception exception = assertThrows(MissingRequiredConfigurationException.class, () -> {
+            new NewRelicMeterRegistry(config, clock);
+        });
+        assertThat(exception.getMessage()).contains("eventType");
+    }
+
+    @Test
+    void configMissingAccountId() {
+        NewRelicConfig config = new NewRelicConfig() {
+            @Override
+            public String eventType() {
+                return "eventType";
+            }
+            @Override
+            public String accountId() {
+                return null;
+            }
+            @Override
+            public String get(String key) {
+                return null;
+            }
+        };
+        
+        Exception exception = assertThrows(MissingRequiredConfigurationException.class, () -> {
+            new NewRelicMeterRegistry(config, clock);
+        });
+        assertThat(exception.getMessage()).contains("accountId");
+    }
     
+    @Test
+    void configMissingApiKey() {
+        NewRelicConfig config = new NewRelicConfig() {
+            @Override
+            public String eventType() {
+                return "eventType";
+            }
+            @Override
+            public String accountId() {
+                return "accountId";
+            }
+            @Override
+            public String apiKey() {
+                return "";
+            }
+            @Override
+            public String get(String key) {
+                return null;
+            }
+        };
+        
+        Exception exception = assertThrows(MissingRequiredConfigurationException.class, () -> {
+            new NewRelicMeterRegistry(config, clock);
+        });
+        assertThat(exception.getMessage()).contains("apiKey");
+    }
+    
+    @Test
+    void configMissingUri() {
+        NewRelicConfig config = new NewRelicConfig() {
+            @Override
+            public String eventType() {
+                return "eventType";
+            }
+            @Override
+            public String accountId() {
+                return "accountId";
+            }
+            @Override
+            public String apiKey() {
+                return "apiKey";
+            }
+            @Override
+            public String uri() {
+                return "";
+            }
+            @Override
+            public String get(String key) {
+                return null;
+            }
+        };
+        
+        Exception exception = assertThrows(MissingRequiredConfigurationException.class, () -> {
+            new NewRelicMeterRegistry(config, clock);
+        });
+        assertThat(exception.getMessage()).contains("uri");
+    }
+    
+    class MockHttpSender implements HttpSender {
+        
+        private Request request;
+        
+        @Override
+        public Response send(Request request) throws Throwable {
+            this.request = request;
+            return new Response(200, "body");
+        }
+        
+        public Request getRequest() {
+            return request;
+        }
+    }
+
 }
