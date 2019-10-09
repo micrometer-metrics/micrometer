a79700e699be842e693ba7e6068b4d607d89bb41
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2019-10-09 16:51:41 -0500

    Account for Double.NaN when creating human readable output

--- micrometer-core/src/main/java/io/micrometer/core/instrument/logging/LoggingMeterRegistry.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/logging/LoggingMeterRegistry.java
@@ -209,7 +209,7 @@ public class LoggingMeterRegistry extends StepMeterRegistry {
         // see https://stackoverflow.com/a/3758880/510017
         String humanReadableByteCount(double bytes) {
             int unit = 1024;
-            if (bytes < unit) return decimalOrNan(bytes) + " B";
+            if (bytes < unit || Double.isNaN(bytes)) return decimalOrNan(bytes) + " B";
             int exp = (int) (Math.log(bytes) / Math.log(unit));
             String pre = "KMGTPE".charAt(exp - 1) + "i";
             return decimalOrNan(bytes / Math.pow(unit, exp)) + " " + pre + "B";
