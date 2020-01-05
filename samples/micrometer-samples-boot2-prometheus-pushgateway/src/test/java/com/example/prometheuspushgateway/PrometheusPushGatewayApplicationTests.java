a46c990aa7e36b4f668f4a572a362d7fdf3bc062
Luram Archanjo <luram.archanjo@sensedia.com>
2020-01-05 09:26:30 -0300

    Revert "Sample Spring boot 2.2 and Prometheus Pushgateway with Micrometer"
    This reverts commit f5d9406347b8a9e7ff86557027c7fa3b54e2f761.

---
+++ samples/micrometer-samples-boot2-prometheus-pushgateway/src/test/java/com/example/prometheuspushgateway/PrometheusPushGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometheuspushgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusPushGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
