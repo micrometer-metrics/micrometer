f4d0108fedfc44957ae7f302d1d21801f697f098
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2020-03-18 22:12:27 +0900

    Rename tags provider classes to indicate Servlet dependency (#1913)

--- micrometer-core/src/main/java/io/micrometer/core/instrument/binder/http/HttpServletRequestTagsProvider.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/binder/http/HttpServletRequestTagsProvider.java
@@ -17,15 +17,24 @@ package io.micrometer.core.instrument.binder.http;
 
 import io.micrometer.core.annotation.Incubating;
 import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Tags;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ *  Provides {@link Tag Tags} for HTTP Servlet request handling.
+ *
+ *  @author Jon Schneider
+ *  @since 1.4.0
+ */
 @Incubating(since = "1.4.0")
-public class DefaultHttpRequestTagsProvider implements HttpRequestTagsProvider {
-    @Override
-    public Iterable<Tag> getTags(HttpServletRequest request, HttpServletResponse response) {
-        return Tags.of(HttpRequestTags.method(request), HttpRequestTags.status(response), HttpRequestTags.outcome(response));
-    }
+public interface HttpServletRequestTagsProvider {
+    /**
+     * Provides tags to be associated with metrics for the given {@code request} and
+     * {@code response} exchange.
+     * @param request the request
+     * @param response the response
+     * @return tags to associate with metrics for the request and response exchange
+     */
+    Iterable<Tag> getTags(HttpServletRequest request, HttpServletResponse response);
 }
