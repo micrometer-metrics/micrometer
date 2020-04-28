bd69afbe06cf518963b8ae20f14b258e97666467
Igor Perikov <IgorPerikov@users.noreply.github.com>
2020-04-28 10:33:05 +0300

    Constructors to customize InstrumentedQueuedThreadPool  (#1658)
    In some cases, like using Spring XML config, the lack of these constructors makes it harder to customize the underlying thread pool without writing code outside of the configuration.
    
    Resolves #1639

--- micrometer-core/src/main/java/io/micrometer/core/instrument/binder/jetty/InstrumentedQueuedThreadPool.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/binder/jetty/InstrumentedQueuedThreadPool.java
@@ -19,12 +19,47 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
+import java.util.concurrent.BlockingQueue;
+
 public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
 
     private final MeterRegistry registry;
     private final Iterable<Tag> tags;
 
     public InstrumentedQueuedThreadPool(MeterRegistry registry, Iterable<Tag> tags) {
+        this.registry = registry;
+        this.tags = tags;
+    }
+
+    public InstrumentedQueuedThreadPool(MeterRegistry registry, Iterable<Tag> tags, int maxThreads) {
+        super(maxThreads);
+        this.registry = registry;
+        this.tags = tags;
+    }
+
+    public InstrumentedQueuedThreadPool(MeterRegistry registry, Iterable<Tag> tags, int maxThreads, int minThreads) {
+        super(maxThreads, minThreads);
+        this.registry = registry;
+        this.tags = tags;
+    }
+
+    public InstrumentedQueuedThreadPool(MeterRegistry registry,
+                                        Iterable<Tag> tags,
+                                        int maxThreads,
+                                        int minThreads,
+                                        int idleTimeout) {
+        super(maxThreads, minThreads, idleTimeout);
+        this.registry = registry;
+        this.tags = tags;
+    }
+
+    public InstrumentedQueuedThreadPool(MeterRegistry registry,
+                                        Iterable<Tag> tags,
+                                        int maxThreads,
+                                        int minThreads,
+                                        int idleTimeout,
+                                        BlockingQueue<Runnable> queue) {
+        super(maxThreads, minThreads, idleTimeout, queue);
         this.registry = registry;
         this.tags = tags;
     }
