458b7f6c9c10cb05bbb44a42b37b1f2d32b0c0a9
arohatgi <amit_rohatgi@intuit.com>
2020-08-03 12:42:53 -0700

    update license

--- implementations/micrometer-registry-statsd/src/main/java/io/micrometer/statsd/internal/StatsdAddressResolverGroup.java
+++ implementations/micrometer-registry-statsd/src/main/java/io/micrometer/statsd/internal/StatsdAddressResolverGroup.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 VMware, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micrometer.statsd.internal;
 
 import io.netty.resolver.AddressResolver;
@@ -6,6 +21,11 @@ import io.netty.resolver.DefaultNameResolver;
 import io.netty.util.concurrent.EventExecutor;
 
 import java.net.InetSocketAddress;
+
+/**
+ * A {@link DefaultNameResolver} that resolves using JDK's built-in domain name lookup mechanism.
+ * Note that this resolver performs a blocking name lookup from the caller thread.
+ */
 
 public final class StatsdAddressResolverGroup extends AddressResolverGroup<InetSocketAddress> {
 
