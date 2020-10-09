510fc303e8871f998a30d65ff372a58c0999d3d2
Erin Schnabel <ebullient@redhat.com>
2020-10-09 15:24:25 -0400

    Begin linkage to Registry TCK

---
+++ implementations/micrometer-registry-opentelemetry/src/test/java/io/micrometer/opentelemetry/OpenTelemetryMeterRegistryCompatibilityTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2017 VMware, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.opentelemetry;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.MockClock;
+import io.micrometer.core.tck.MeterRegistryCompatibilityKit;
+import io.opentelemetry.exporters.inmemory.InMemoryMetricExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.export.IntervalMetricReader;
+
+class OpenTelemetryMeterRegistryCompatibilityTest extends MeterRegistryCompatibilityKit {
+    @Override
+    public MeterRegistry registry() {
+        OpenTelemetryRegistry registry = new OpenTelemetryRegistry(OpenTelemetryConfig.DEFAULT, new MockClock());
+        InMemoryMetricExporter exporter = InMemoryMetricExporter.create();
+        IntervalMetricReader intervalMetricReader = IntervalMetricReader.builder()
+                .setMetricExporter(exporter)
+                .setMetricProducers(Collections.singletonList(OpenTelemetrySdk.getMeterProvider().getMetricProducer()))
+                .setExportIntervalMillis(1000)
+                .build();
+        return registry;
+    }
+
+    @Override
+    public Duration step() {
+        return Duration.ofMinutes(1);
+    }
+}
