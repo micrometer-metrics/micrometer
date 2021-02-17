8f117bacd70e8f6900cf66072b4bb0c25b28ff66
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2021-02-17 17:23:52 +0900

    Attribute nullability annotations to Spring Framework (#2456)
    Includes a note in the class-level JavaDoc for each class, adds the `@author` tags from the original files, and adds an entry to the top-level NOTICE file.

--- micrometer-core/src/main/java/io/micrometer/core/lang/NonNullFields.java
+++ micrometer-core/src/main/java/io/micrometer/core/lang/NonNullFields.java
@@ -26,7 +26,10 @@ import java.lang.annotation.*;
  * tools with JSR-305 support and used by Kotlin to infer nullability of the API.
  * <p>Should be used at package level in association with {@link Nullable}
  * annotations at field level.
+ * <p>
+ * NOTE: This file has been copied from {org.springframework.lang}.
  *
+ * @author Sebastien Deleuze
  * @see NonNullFields
  * @see Nullable
  * @see NonNull
