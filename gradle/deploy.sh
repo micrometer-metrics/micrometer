bbfb0c8cd24cb28bcbada822515d53a5eed3b0bf
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-03-09 14:41:31 -0800

    Updating deploy.sh with the new tasks, also simplifying it

--- gradle/deploy.sh
+++ gradle/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # This script will build the project.
 
-SWITCHES="-s --console=plain"
+SWITCHES="-s --console=plain -x release -x artifactoryPublish -x bintrayUpload -x bintrayPublish"
 
 if [ $CIRCLE_PR_NUMBER ]; then
   echo -e "WARN: Should not be here => Found Pull Request #$CIRCLE_PR_NUMBER => Branch [$CIRCLE_BRANCH]"
@@ -9,16 +9,16 @@ if [ $CIRCLE_PR_NUMBER ]; then
 elif [ -z $CIRCLE_TAG ]; then
   echo -e "Publishing Snapshot => Branch ['$CIRCLE_BRANCH']"
   openssl aes-256-cbc -d -in gradle.properties.enc -out gradle.properties -k "$KEY" -md sha256
-  ./gradlew snapshot $SWITCHES -x release -x test
+  ./gradlew snapshot publishNebulaPublicationToSnapshotRepository "$SWITCHES" -x test
 elif [ $CIRCLE_TAG ]; then
   echo -e "Publishing Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']"
   openssl aes-256-cbc -d -in gradle.properties.enc -out gradle.properties -k "$KEY" -md sha256
   case "$CIRCLE_TAG" in
-  *-rc\.*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate publishAllPublicationsToMilestoneRepository $SWITCHES -x release -x artifactoryPublish -x bintrayUpload -x bintrayPublish
+  *-M\.*)
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate publishNebulaPublicationToMilestoneRepository "$SWITCHES"
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final publishAllPublicationsToMavenCentralRepository $SWITCHES -x release -x artifactoryPublish -x bintrayUpload -x bintrayPublish
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final publishNebulaPublicationToMavenCentralRepository closeAndReleaseMavenCentralStagingRepository "$SWITCHES"
     ;;
   esac
 else
