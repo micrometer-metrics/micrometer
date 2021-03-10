725eb03d7cb117d706dfb13e163f4b781cd445e6
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-03-10 12:33:31 -0800

    Bintray Sunset (#2487)
    * Setup publication signing and maven central publish (bypassing the artifactory and bintray plugins)
    
    * Setup publishing to milestone and snapshot repos
    
    * Modifying deploy script
    
    * Adding io.github.gradle-nexus.publish-plugin so that we can close and release maven central repos from gradle
    
    * Deleting encrypted property file

--- gradle/deploy.sh
+++ gradle/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # This script will build the project.
 
-SWITCHES="-s --console=plain"
+SWITCHES="-s --console=plain -x release -x artifactoryPublish -x bintrayUpload -x bintrayPublish"
 
 if [ $CIRCLE_PR_NUMBER ]; then
   echo -e "WARN: Should not be here => Found Pull Request #$CIRCLE_PR_NUMBER => Branch [$CIRCLE_BRANCH]"
@@ -9,18 +9,16 @@ if [ $CIRCLE_PR_NUMBER ]; then
 elif [ -z $CIRCLE_TAG ]; then
   echo -e "Publishing Snapshot => Branch ['$CIRCLE_BRANCH']"
   openssl aes-256-cbc -d -in gradle.properties.enc -out gradle.properties -k "$KEY" -md sha256
-  ./gradlew snapshot $SWITCHES -x release -x test
+  ./gradlew snapshot publishNebulaPublicationToSnapshotRepository "$SWITCHES" -x test
 elif [ $CIRCLE_TAG ]; then
   echo -e "Publishing Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']"
   openssl aes-256-cbc -d -in gradle.properties.enc -out gradle.properties -k "$KEY" -md sha256
   case "$CIRCLE_TAG" in
-  *-rc\.*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate bintrayUpload $SWITCHES -x release -x bintrayPublish
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true :micrometer-core:bintrayPublish $SWITCHES -x bintrayUpload
+  *-M\.*)
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate publishNebulaPublicationToMilestoneRepository "$SWITCHES"
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final bintrayUpload $SWITCHES -x release -x artifactoryPublish -x bintrayPublish
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true :micrometer-core:bintrayPublish $SWITCHES -x bintrayUpload
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final publishNebulaPublicationToMavenCentralRepository closeAndReleaseMavenCentralStagingRepository "$SWITCHES"
     ;;
   esac
 else
