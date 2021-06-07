c0ddb0a543a4d8029253e854d7b576f69f10b590
izeye <izeye@naver.com>
2021-06-07 14:22:45 +0900

    Replace deprecated HealthStatusManager references

--- samples/micrometer-samples-core/src/main/java/io/micrometer/core/samples/GrpcMetricsSample.java
+++ samples/micrometer-samples-core/src/main/java/io/micrometer/core/samples/GrpcMetricsSample.java
@@ -25,7 +25,7 @@ import io.grpc.health.v1.HealthGrpc;
 import io.grpc.health.v1.HealthGrpc.HealthBlockingStub;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
-import io.grpc.services.HealthStatusManager;
+import io.grpc.protobuf.services.HealthStatusManager;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.grpc.MetricCollectingClientInterceptor;
