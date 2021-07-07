4f89bbbfbd36a378339000f740e0049c7f891ea0
Johnny Lim <izeye@naver.com>
2021-07-08 03:00:28 +0900

    Polish logging test support (#2689)

--- micrometer-test/src/main/java/io/micrometer/core/util/internal/logging/MockLoggerFactory.java
+++ micrometer-test/src/main/java/io/micrometer/core/util/internal/logging/MockLoggerFactory.java
@@ -16,48 +16,50 @@
 package io.micrometer.core.util.internal.logging;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Supplier;
 
 /**
- * Simple {@link InternalLoggerFactory} implementation that always returns an instance of {@link InternalMockLogger}
+ * Simple {@link InternalLoggerFactory} implementation that always returns an instance of {@link MockLogger}
  * so components that fetch the logger this way: <code>InternalLogger logger = InternalLoggerFactory.getInstance(MyClass.class);</code>,
- * get a {@link InternalMockLogger} instance if they are created using <code>factory.injectLogger(MyClass::new)</code>
- * or if <code>InternalLoggerFactory.setDefaultFactory(internalMockLoggerFactory);</code> was set previously.
+ * get a {@link MockLogger} instance if they are created using <code>factory.injectLogger(MyClass::new)</code>
+ * or if <code>InternalLoggerFactory.setDefaultFactory(mockLoggerFactory);</code> was set previously.
  *
  * @author Jonatan Ivanov
  */
-public class InternalMockLoggerFactory extends InternalLoggerFactory {
-    private final ConcurrentHashMap<String, InternalMockLogger> loggers = new ConcurrentHashMap<>();
+public class MockLoggerFactory extends InternalLoggerFactory {
+    private final ConcurrentMap<String, MockLogger> loggers = new ConcurrentHashMap<>();
 
     /**
-     * A convenient way to create a logger. In contrast of getInstance, this method returns a {@link InternalMockLogger}
-     * instead of a {@link InternalLoggerFactory} so that you don't need to cast.
+     * A convenient way to create a logger. In contrast of {@link #getInstance(Class)}, this method returns a {@link MockLogger}
+     * instead of an {@link InternalLoggerFactory} so that you don't need to cast.
      *
      * @param clazz The class that you need the logger for.
-     * @return An instance of {@link InternalMockLogger} for the provided class.
+     * @return An instance of {@link MockLogger} for the provided class.
      */
-    public InternalMockLogger getLogger(Class<?> clazz) {
+    public MockLogger getLogger(Class<?> clazz) {
         return getLogger(clazz.getName());
     }
 
     /**
-     * A convenient way to create a logger. In contrast of getInstance, this method returns a {@link InternalMockLogger}
-     * instead of a {@link InternalLoggerFactory} so that you don't need to cast.
+     * A convenient way to create a logger. In contrast of {@link #getInstance(String)}, this method returns a {@link MockLogger}
+     * instead of an {@link InternalLoggerFactory} so that you don't need to cast.
      *
      * @param name The name of the logger.
-     * @return An instance of {@link InternalMockLogger} for the provided name.
+     * @return An instance of {@link MockLogger} for the provided name.
      */
-    public InternalMockLogger getLogger(String name) {
-        return loggers.computeIfAbsent(name, InternalMockLogger::new);
+    public MockLogger getLogger(String name) {
+        return loggers.computeIfAbsent(name, MockLogger::new);
     }
 
     /**
-     * A factory method that returns the result of the given Supplier and it also injects the default factory.
+     * A factory method that returns the result of the given {@code Supplier} and it also injects the default factory.
      * So if the logger is created this way: <code>InternalLogger logger = InternalLoggerFactory.getInstance(MyClass.class);</code>,
-     * the resulted logger will be a {@link InternalMockLogger}.
+     * the resulted logger will be a {@link MockLogger}.
      *
      * @param supplier The logic that creates the object you need to inject the logger into.
-     * @return The result of the Supplier.
+     * @param <T> The type of an object that will be created by the {@code Supplier}.
+     * @return The result of the {@code Supplier}.
      */
     public <T> T injectLogger(Supplier<T> supplier) {
         synchronized (this) {
