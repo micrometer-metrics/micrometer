15c2eaf32687cb2519da0a219bd0c507651c53c2
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2021-09-02 18:32:52 +0900

    Add Jersey 3 sample
    This demonstrates that the micrometer-jersey2 module can be used with Jersey 3 also.

---
+++ samples/micrometer-samples-jersey3/src/main/java/io/micrometer/samples/jersey3/HelloWorldResource.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2021 VMware, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.samples.jersey3;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+
+@Path("hello/{name}")
+public class HelloWorldResource {
+
+    @GET
+    @Produces("text/plain")
+    public String sayHi(@PathParam("name") String name) {
+        return "Hello, " + name;
+    }
+}
