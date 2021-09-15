f5538c7a8e9d5ae22ed8daafa6f8eeb339d5c1dd
Stefan Birkner <mail@stefan-birkner.de>
2021-09-15 15:54:12 +0200

    Support Jersey 3 in micrometer-jersey2 (#2766)
    Jersey 3 is using classes from the jakarta.* packages instead of javax.*. The Jersey 2 code can be used with Jersey 3 if we don't import these classes.
    
    This avoids importing javax.* classes by inlining code, not storing variables and checking for `NotFoundException` by its class name. Checkstyle rules have been updated to forbid `javax.ws.*` imports to avoid mistakenly updating the code later.
    
    A new sample demonstrates that the micrometer-jersey2 module can be used with Jersey 3 also.
    
    Co-authored-by: Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>

---
+++ samples/micrometer-samples-jersey3/src/test/java/io/micrometer/samples/jersey3/Jersey3Test.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2021 VMware, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.samples.jersey3;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.micrometer.jersey2.server.DefaultJerseyTagsProvider;
+import io.micrometer.jersey2.server.MetricsApplicationEventListener;
+import jakarta.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Jersey3Test extends JerseyTest {
+
+    static final String TIMER_METRIC_NAME = "http.server.requests";
+    MeterRegistry registry;
+
+    @Override
+    protected Application configure() {
+        registry = new SimpleMeterRegistry();
+        MetricsApplicationEventListener metricsListener = new MetricsApplicationEventListener(registry, new DefaultJerseyTagsProvider(), TIMER_METRIC_NAME, true);
+        return new ResourceConfig(HelloWorldResource.class)
+                .register(metricsListener);
+    }
+
+    @Test
+    public void helloResourceIsTimed() {
+        String response = target("hello/Jersey").request().get(String.class);
+        assertThat(response).isEqualTo("Hello, Jersey");
+        Timer timer = registry.get(TIMER_METRIC_NAME)
+                .tags("method", "GET", "uri", "/hello/{name}", "status", "200", "exception", "None", "outcome", "SUCCESS")
+                .timer();
+        assertThat(timer.count()).isEqualTo(1);
+        assertThat(timer.totalTime(TimeUnit.NANOSECONDS)).isPositive();
+    }
+}
