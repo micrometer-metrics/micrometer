01b23a93e6533cc2d8b16910800c70a1b2372828
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2021-10-14 18:02:18 +0200

    Report 404 responses with the uri if a resource was matched (#2587)
    If a resource was matched and the application logic returns a 404, we are okay to tag the URI since it is a discrete resource and not an unbounded unmapped URI.

--- micrometer-jersey2/src/main/java/io/micrometer/jersey2/server/JerseyTags.java
+++ micrometer-jersey2/src/main/java/io/micrometer/jersey2/server/JerseyTags.java
@@ -94,7 +94,7 @@ public final class JerseyTags {
             if (isRedirection(status)) {
                 return URI_REDIRECTION;
             }
-            if (status == 404) {
+            if (status == 404 && event.getUriInfo().getMatchedResourceMethod() == null) {
                 return URI_NOT_FOUND;
             }
         }
