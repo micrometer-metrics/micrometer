8b4964e71d7e318033144c48da188cf372e49489
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-01-04 16:58:50 -0800

    Regenerate license headers using the license plugin for 1.8.x related: gh-2942 gh-2412 gh-2940

--- implementations/micrometer-registry-dynatrace/src/main/java/io/micrometer/dynatrace/v1/DynatraceExporterV1.java
+++ implementations/micrometer-registry-dynatrace/src/main/java/io/micrometer/dynatrace/v1/DynatraceExporterV1.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.micrometer.dynatrace.v1;
 
 import io.micrometer.core.instrument.*;
--- implementations/micrometer-registry-dynatrace/src/main/java/io/micrometer/dynatrace/v2/DynatraceExporterV2.java
+++ implementations/micrometer-registry-dynatrace/src/main/java/io/micrometer/dynatrace/v2/DynatraceExporterV2.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.micrometer.dynatrace.v2;
 
 import com.dynatrace.metric.util.*;
