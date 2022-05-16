15e4391afb8789c7a1acc9a6d44c5259c8ef9517
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-05-15 21:55:15 -0700

    Change package name of Observation TCK (#3178)
    Observation TCK should be in the io.micrometer.observation.tck package instead of io.micrometer.core.tck.
    micrometer-test already has the io.micrometer.core.tck package so this change also resolves potential issues with the module system on Java 9 and above.

--- micrometer-observation-test/src/main/java/io/micrometer/observation/tck/ObservationRegistryCompatibilityKit.java
+++ micrometer-observation-test/src/main/java/io/micrometer/observation/tck/ObservationRegistryCompatibilityKit.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micrometer.core.tck;
+package io.micrometer.observation.tck;
 
 import io.micrometer.common.KeyValue;
 import io.micrometer.common.KeyValues;
