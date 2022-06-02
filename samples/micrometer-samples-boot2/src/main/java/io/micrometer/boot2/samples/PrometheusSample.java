2bf6285845c35cbf39e795d5def3a0d571085291
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-06-02 13:29:06 -0700

    Fixing imports

--- samples/micrometer-samples-boot2/src/main/java/io/micrometer/boot2/samples/PrometheusSample.java
+++ samples/micrometer-samples-boot2/src/main/java/io/micrometer/boot2/samples/PrometheusSample.java
@@ -16,16 +16,8 @@
 package io.micrometer.boot2.samples;
 
 import io.micrometer.boot2.samples.components.PersonController;
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.exemplars.DefaultExemplarSampler;
-import io.prometheus.client.exemplars.ExemplarSampler;
-import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackageClasses = PersonController.class)
