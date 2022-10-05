67e3061d78b3b49502cc7c07f3fae294a2cd3991
Joel Costa <frederico08@gmail.com>
2022-10-05 18:28:46 +0100

    Remove deprecated @nullable

--- implementations/micrometer-registry-appdynamics/src/main/java/io/github/micrometer/appdynamics/PathNamingConvention.java
+++ implementations/micrometer-registry-appdynamics/src/main/java/io/github/micrometer/appdynamics/PathNamingConvention.java
@@ -2,7 +2,6 @@ package io.github.micrometer.appdynamics;
 
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.config.NamingConvention;
-import io.micrometer.core.lang.Nullable;
 
 /**
  * @author Ricardo Veloso
@@ -16,7 +15,8 @@ public class PathNamingConvention implements NamingConvention {
     }
 
     @Override
-    public String name(String name, Meter.Type type, @Nullable String baseUnit) {
+    public String name(String name, Meter.Type type, String baseUnit) {
         return config.prefix() + "|" + name;
     }
+
 }
