0602b39e2ead82aa33f72f2aa53d149831718018
Johnny Lim <izeye@naver.com>
2023-03-22 22:25:35 +0900

    Add supplier variant for Observation.Context.getOrDefault()

--- micrometer-observation/src/main/java/io/micrometer/observation/Observation.java
+++ micrometer-observation/src/main/java/io/micrometer/observation/Observation.java
@@ -1295,6 +1295,18 @@ public interface Observation extends ObservationView {
         <T> T getOrDefault(Object key, T defaultObject);
 
         /**
+         * Returns an element or default if not present.
+         * @param key key
+         * @param defaultObjectSupplier supplier for default object to return
+         * @param <T> value type
+         * @return object or default if not present
+         */
+        default <T> T getOrDefault(Object key, Supplier<T> defaultObjectSupplier) {
+            T value = get(key);
+            return value != null ? value : defaultObjectSupplier.get();
+        }
+
+        /**
          * Returns low cardinality key values.
          * @return low cardinality key values
          */
