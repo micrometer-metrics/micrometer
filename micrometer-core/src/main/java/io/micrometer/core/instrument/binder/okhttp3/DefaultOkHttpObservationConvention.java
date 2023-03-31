888f2406ea9d337f72ae3ea66836bee667b36cd9
kubamarchwicki <kuba@marchwicki.pl>
2023-03-31 20:15:28 +0200

    Add outcome tag to OkHttp metrics

--- micrometer-core/src/main/java/io/micrometer/core/instrument/binder/okhttp3/DefaultOkHttpObservationConvention.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/binder/okhttp3/DefaultOkHttpObservationConvention.java
@@ -22,6 +22,7 @@ import io.micrometer.common.lang.NonNullFields;
 import io.micrometer.common.lang.Nullable;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.http.Outcome;
 import okhttp3.Request;
 import okhttp3.Response;
 
@@ -88,7 +89,8 @@ public class DefaultOkHttpObservationConvention implements OkHttpObservationConv
         KeyValues keyValues = KeyValues
             .of(METHOD.withValue(requestAvailable ? request.method() : TAG_VALUE_UNKNOWN),
                     URI.withValue(getUriTag(urlMapper, state, request)),
-                    STATUS.withValue(getStatusMessage(state.response, state.exception)))
+                    STATUS.withValue(getStatusMessage(state.response, state.exception)),
+                    OUTCOME.withValue(getStatusOutcome(state.response).name()))
             .and(extraTags)
             .and(stream(contextSpecificTags.spliterator(), false)
                 .map(contextTag -> contextTag.apply(request, state.response))
@@ -110,6 +112,14 @@ public class DefaultOkHttpObservationConvention implements OkHttpObservationConv
         }
         return state.response != null && (state.response.code() == 404 || state.response.code() == 301) ? "NOT_FOUND"
                 : urlMapper.apply(request);
+    }
+
+    private Outcome getStatusOutcome(@Nullable Response response) {
+        if (response == null) {
+            return Outcome.UNKNOWN;
+        }
+
+        return Outcome.forStatus(response.code());
     }
 
     private String getStatusMessage(@Nullable Response response, @Nullable IOException exception) {
