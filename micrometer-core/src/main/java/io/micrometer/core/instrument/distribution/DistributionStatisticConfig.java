059b9ab76319876fdb188f4e6b405de16a64af0c
gregor <gregor.zeitlinger@grafana.com>
2023-06-29 17:29:32 +0200

    Add method to turn DistributionStatisticConfig into a builder - if you just want to change a single property from the default

--- micrometer-core/src/main/java/io/micrometer/core/instrument/distribution/DistributionStatisticConfig.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/distribution/DistributionStatisticConfig.java
@@ -72,7 +72,11 @@ public class DistributionStatisticConfig implements Mergeable<DistributionStatis
     private Integer bufferLength;
 
     public static Builder builder() {
-        return new Builder();
+        return new Builder(new DistributionStatisticConfig());
+    }
+
+    public Builder toBuilder() {
+        return new Builder(this);
     }
 
     /**
@@ -271,7 +275,11 @@ public class DistributionStatisticConfig implements Mergeable<DistributionStatis
 
     public static class Builder {
 
-        private final DistributionStatisticConfig config = new DistributionStatisticConfig();
+        private final DistributionStatisticConfig config;
+
+        public Builder(DistributionStatisticConfig config) {
+            this.config = config;
+        }
 
         public Builder percentilesHistogram(@Nullable Boolean enabled) {
             config.percentileHistogram = enabled;
