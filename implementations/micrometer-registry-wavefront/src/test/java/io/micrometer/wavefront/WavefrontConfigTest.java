7157fc7f3a5d3bcc574afae688c1c113b0dba3b2
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-09-28 10:52:13 -0700

    Support API Token Type from the Wavefront SDK

--- implementations/micrometer-registry-wavefront/src/test/java/io/micrometer/wavefront/WavefrontConfigTest.java
+++ implementations/micrometer-registry-wavefront/src/test/java/io/micrometer/wavefront/WavefrontConfigTest.java
@@ -15,13 +15,16 @@
  */
 package io.micrometer.wavefront;
 
+import com.wavefront.sdk.common.clients.service.token.TokenService.Type;
 import io.micrometer.core.Issue;
 import io.micrometer.core.instrument.config.validate.ValidationException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class WavefrontConfigTest {
@@ -42,6 +45,52 @@ class WavefrontConfigTest {
         WavefrontConfig config = wavefrontProps::getProperty;
         assertThatCode(config::uri).isExactlyInstanceOf(ValidationException.class)
             .hasMessageContaining("it must be a valid URI");
+    }
+
+    @Test
+    void noExceptionAndNoTokenTypeIfTokenMissingButProxyUsed() {
+        Map<String, String> wavefrontProps = Map.of("wavefront.uri", "proxy://example.org:2878");
+        WavefrontConfig config = wavefrontProps::get;
+        assertThatCode(config::apiToken).doesNotThrowAnyException();
+        assertThat(config.apiTokenType()).isSameAs(Type.NO_TOKEN);
+    }
+
+    @Test
+    void noExceptionAndWavefrontTokenTypeIfTokenPresentAndDirectAccessUsed() {
+        Map<String, String> wavefrontProps = Map.of("wavefront.uri", "https://example.org:2878", "wavefront.apiToken",
+                "s3cr3t");
+        WavefrontConfig config = wavefrontProps::get;
+        assertThatCode(config::apiToken).doesNotThrowAnyException();
+        assertThat(config.apiTokenType()).isSameAs(Type.WAVEFRONT_API_TOKEN);
+    }
+
+    @Test
+    void apiTokenFailsIfNoTokenPresentAndDirectAccessUsed() {
+        Map<String, String> wavefrontProps = Map.of("wavefront.uri", "https://example.org:2878");
+        WavefrontConfig config = wavefrontProps::get;
+        assertThatCode(config::apiToken).isExactlyInstanceOf(ValidationException.class)
+            .hasNoCause()
+            .hasMessage(
+                    "wavefront.apiToken was 'null' but it must be set whenever publishing directly to the Wavefront API");
+    }
+
+    @Test
+    void apiTokenTypeFailsIfNoTokenTypeAndDirectAccessUsed() {
+        Map<String, String> wavefrontProps = Map.of("wavefront.uri", "https://example.org:2878",
+                "wavefront.apiTokenType", Type.NO_TOKEN.name());
+        WavefrontConfig config = wavefrontProps::get;
+        assertThatCode(config::apiTokenType).isExactlyInstanceOf(ValidationException.class)
+            .hasNoCause()
+            .hasMessage(
+                    "wavefront.apiTokenType was 'No-Op/Proxy' but it must be set to something else whenever publishing directly to the Wavefront API");
+    }
+
+    @Test
+    void apiTokenTypeShouldBeUsedIfDirectAccessUsed() {
+        Map<String, String> wavefrontProps = Map.of("wavefront.uri", "https://example.org:2878",
+                "wavefront.apiTokenType", Type.CSP_API_TOKEN.name());
+        WavefrontConfig config = wavefrontProps::get;
+        assertThat(config.apiTokenType()).isSameAs(Type.CSP_API_TOKEN);
     }
 
 }
