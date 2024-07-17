5fde87bfea3666c33188461133f95f0b1b2dd75a
Fábio C. Martins <fabio.martins@serpro.gov.br>
2024-07-17 10:26:58 -0300

    Binders for the environment memory metrics the application is running in

--- micrometer-core/src/test/java/io/micrometer/core/instrument/binder/system/MemoryMetricsTest.java
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/binder/system/MemoryMetricsTest.java
@@ -15,8 +15,6 @@
  */
 package io.micrometer.core.instrument.binder.system;
 
-import io.micrometer.common.util.internal.logging.InternalLogger;
-import io.micrometer.common.util.internal.logging.InternalLoggerFactory;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.BaseUnits;
@@ -24,10 +22,7 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -37,8 +32,6 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
  * @author Fábio C. Martins
  */
 class MemoryMetricsTest {
-    private final InternalLogger logger = InternalLoggerFactory.getInstance(MemoryMetricsTest.class);
-
     MeterRegistry registry = new SimpleMeterRegistry();
 
     @BeforeEach
@@ -49,47 +42,29 @@ class MemoryMetricsTest {
     @Test
     void memoryMetrics() {
         Gauge virtualMemCommited = registry.get("system.virtualmemory.commited").gauge();
-        assertThat(virtualMemCommited.value()).isGreaterThanOrEqualTo(0);
         assertThat(virtualMemCommited.getId().getBaseUnit()).isEqualTo(BaseUnits.BYTES);
-        logger.info("{}", virtualMemCommited.value());
 
         Gauge swapTotal = registry.get("system.swap.total").gauge();
-        assertThat(swapTotal.value()).isGreaterThanOrEqualTo(0);
+        assertThat(swapTotal.value()).isNotNegative();
         assertThat(swapTotal.getId().getBaseUnit()).isEqualTo(BaseUnits.BYTES);
-        logger.info("{}", swapTotal.value());
 
         Gauge swapFree = registry.get("system.swap.free").gauge();
-        assertThat(swapFree.value()).isGreaterThanOrEqualTo(0);
+        assertThat(swapFree.value()).isNotNegative();
         assertThat(swapFree.getId().getBaseUnit()).isEqualTo(BaseUnits.BYTES);
-        logger.info("{}", swapFree.value());
 
         Gauge memFree = registry.get("system.memory.free").gauge();
-        assertThat(memFree.value()).isGreaterThanOrEqualTo(0);
         assertThat(memFree.getId().getBaseUnit()).isEqualTo(BaseUnits.BYTES);
-        logger.info("{}", memFree.value());
 
         Gauge memTotal = registry.get("system.memory.total").gauge();
-        assertThat(memTotal.value()).isGreaterThanOrEqualTo(0);
+        assertThat(memTotal.value()).isNotNegative();
         assertThat(memTotal.getId().getBaseUnit()).isEqualTo(BaseUnits.BYTES);
-        logger.info("{}", memTotal.value());
-
-        await().atMost(Duration.ofMillis(5000));
-        logger.info("{}", virtualMemCommited.value());
-        logger.info("{}", swapTotal.value());
-        logger.info("{}", swapFree.value());
-        logger.info("{}", memFree.value());
-        logger.info("{}", memTotal.value());
     }
 
     @Test
     void hotspotMetrics() {
         assumeFalse(isOpenJ9());
-
         assertThat(registry.get("system.virtualmemory.commited").gauge().value()).isNotNegative();
-        assertThat(registry.get("system.swap.total").gauge().value()).isNotNegative();
-        assertThat(registry.get("system.swap.free").gauge().value()).isNotNegative();
         assertThat(registry.get("system.memory.free").gauge().value()).isNotNegative();
-        assertThat(registry.get("system.memory.total").gauge().value()).isNotNegative();
     }
 
     @Test
@@ -97,10 +72,7 @@ class MemoryMetricsTest {
         assumeTrue(isOpenJ9());
 
         assertThat(registry.get("system.virtualmemory.commited").gauge().value()).isGreaterThanOrEqualTo(-1);
-        assertThat(registry.get("system.swap.total").gauge().value()).isNotNegative();
-        assertThat(registry.get("system.swap.free").gauge().value()).isNotNegative();
         assertThat(registry.get("system.memory.free").gauge().value()).isGreaterThanOrEqualTo(-1);
-        assertThat(registry.get("system.memory.total").gauge().value()).isNotNegative();
     }
 
     private boolean isOpenJ9() {
