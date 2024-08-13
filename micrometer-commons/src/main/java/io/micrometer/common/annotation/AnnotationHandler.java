6b5eb029a9152c39262e3f62c7682347810f05a7
Maksym Symonov <simdenmax@gmail.com>
2024-08-13 10:50:32 +0300

    Improve MeterTag to allow extracting multiple keys/value tags (#5292)
    Makes the `MeterTag` annotation repeatable so that multiple tags can be extracted from a single parameter.
    
    Closes gh-4081

--- micrometer-commons/src/main/java/io/micrometer/common/annotation/AnnotationHandler.java
+++ micrometer-commons/src/main/java/io/micrometer/common/annotation/AnnotationHandler.java
@@ -25,9 +25,12 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * This class is able to find all methods annotated with the Micrometer annotations. All
@@ -118,7 +121,12 @@ public class AnnotationHandler<T> {
                 if (methodsAreTheSame(mostSpecificMethod, methodFromInterface)) {
                     List<AnnotatedParameter> annotatedParametersForActualMethod = AnnotationUtils
                         .findAnnotatedParameters(annotationClass, methodFromInterface, pjp.getArgs());
-                    mergeAnnotatedParameters(annotatedParameters, annotatedParametersForActualMethod);
+                    // annotations for a single parameter can be `duplicated` by the ones
+                    // from parent interface,
+                    // however later on during key-based deduplication the ones from
+                    // specific method(target class)
+                    // will take precedence
+                    annotatedParameters.addAll(annotatedParametersForActualMethod);
                 }
             }
         }
@@ -129,28 +137,16 @@ public class AnnotationHandler<T> {
                 && Arrays.equals(method.getParameterTypes(), mostSpecificMethod.getParameterTypes());
     }
 
-    private void mergeAnnotatedParameters(List<AnnotatedParameter> annotatedParameters,
-            List<AnnotatedParameter> annotatedParametersForActualMethod) {
-        for (AnnotatedParameter container : annotatedParametersForActualMethod) {
-            final int index = container.parameterIndex;
-            boolean parameterContained = false;
-            for (AnnotatedParameter parameterContainer : annotatedParameters) {
-                if (parameterContainer.parameterIndex == index) {
-                    parameterContained = true;
-                    break;
-                }
-            }
-            if (!parameterContained) {
-                annotatedParameters.add(container);
-            }
-        }
+    private void addAnnotatedArguments(T objectToModify, List<AnnotatedParameter> toBeAdded) {
+        toBeAdded.stream()
+            .map(container -> toKeyValue.apply(container.annotation, container.argument))
+            .filter(distinctByKey(KeyValue::getKey))
+            .forEach(keyValue -> keyValueConsumer.accept(keyValue, objectToModify));
     }
 
-    private void addAnnotatedArguments(T objectToModify, List<AnnotatedParameter> toBeAdded) {
-        for (AnnotatedParameter container : toBeAdded) {
-            KeyValue keyValue = toKeyValue.apply(container.annotation, container.argument);
-            keyValueConsumer.accept(keyValue, objectToModify);
-        }
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return item -> seen.putIfAbsent(keyExtractor.apply(item), Boolean.TRUE) == null;
     }
 
     public Function<Class<? extends ValueResolver>, ? extends ValueResolver> getResolverProvider() {
---
+++ micrometer-core/src/main/java/io/micrometer/core/aop/MeterTags.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.core.aop;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Container annotation that aggregates several {@link MeterTag} annotations.
+ *
+ * Can be used natively, declaring several nested {@link MeterTag} annotations. Can also
+ * be used in conjunction with Java 8's support for repeatable annotations, where
+ * {@link MeterTag} can simply be declared several times on the same parameter, implicitly
+ * generating this container annotation.
+ *
+ * @author Maksym Symonov
+ * @author Marcin Grzejszczak
+ * @since 1.14.0
+ * @see MeterTag
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Target(ElementType.PARAMETER)
+@Documented
+public @interface MeterTags {
+
+    MeterTag[] value();
+
+}
