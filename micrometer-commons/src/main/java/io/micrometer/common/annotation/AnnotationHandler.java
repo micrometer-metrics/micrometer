c79596b4232687343b9f77d5d82c1100bffbc57b
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2024-08-13 16:33:08 +0900

    Make distinctByKey private (internal)

--- micrometer-commons/src/main/java/io/micrometer/common/annotation/AnnotationHandler.java
+++ micrometer-commons/src/main/java/io/micrometer/common/annotation/AnnotationHandler.java
@@ -25,11 +25,12 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-
-import static io.micrometer.common.util.StreamUtils.distinctByKey;
+import java.util.function.Predicate;
 
 /**
  * This class is able to find all methods annotated with the Micrometer annotations. All
@@ -141,6 +142,11 @@ public class AnnotationHandler<T> {
             .map(container -> toKeyValue.apply(container.annotation, container.argument))
             .filter(distinctByKey(KeyValue::getKey))
             .forEach(keyValue -> keyValueConsumer.accept(keyValue, objectToModify));
+    }
+
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return item -> seen.putIfAbsent(keyExtractor.apply(item), Boolean.TRUE) == null;
     }
 
     public Function<Class<? extends ValueResolver>, ? extends ValueResolver> getResolverProvider() {
