885ff680628dedbdfd1e1bb08e47d9cbb7899906
Marina Moiseenko <marina.va.moiseenko@gmail.com>
2024-10-11 14:14:22 +0100

    Deleted unused imports

--- samples/micrometer-samples-spring-framework6/src/test/java/io/micrometer/samples/spring6/aop/CountedAspectTest.java
+++ samples/micrometer-samples-spring-framework6/src/test/java/io/micrometer/samples/spring6/aop/CountedAspectTest.java
@@ -21,7 +21,6 @@ import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.aop.CountedAspect;
 import io.micrometer.core.aop.CountedMeterTagAnnotationHandler;
 import io.micrometer.core.aop.MeterTag;
-import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.search.MeterNotFoundException;
