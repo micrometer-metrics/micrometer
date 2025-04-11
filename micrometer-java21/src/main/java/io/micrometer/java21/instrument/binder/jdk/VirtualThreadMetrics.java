09e26cd17122d9f7c273b679f1f27893fea90ea3
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-04-12 01:54:50 +0900

    VirtualThreadSchedulerMXBean metrics (#6104)
    Adds virtual thread metrics based on the newly introduced MBean in Java 24.

--- micrometer-java21/src/main/java/io/micrometer/java21/instrument/binder/jdk/VirtualThreadMetrics.java
+++ micrometer-java21/src/main/java/io/micrometer/java21/instrument/binder/jdk/VirtualThreadMetrics.java
@@ -15,24 +15,28 @@
  */
 package io.micrometer.java21.instrument.binder.jdk;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.*;
+import io.micrometer.core.instrument.binder.BaseUnits;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import jdk.jfr.consumer.RecordingStream;
 
 import java.io.Closeable;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.management.ManagementFactory;
+import java.lang.management.PlatformManagedObject;
 import java.time.Duration;
 import java.util.Objects;
 
 import static java.util.Collections.emptyList;
 
 /**
- * Instrumentation support for Virtual Threads, see:
- * https://openjdk.org/jeps/425#JDK-Flight-Recorder-JFR
+ * Metrics instrumentation for Java Virtual Threads.
  *
  * @author Artyom Gabeev
+ * @see <a href="https://openjdk.org/jeps/425#JDK-Flight-Recorder-JFR">JEP 425</a>
+ * @see VirtualThreadSchedulerMXBean
  * @since 1.14.0
  */
 public class VirtualThreadMetrics implements MeterBinder, Closeable {
@@ -40,6 +44,10 @@ public class VirtualThreadMetrics implements MeterBinder, Closeable {
     private static final String PINNED_EVENT = "jdk.VirtualThreadPinned";
 
     private static final String SUBMIT_FAILED_EVENT = "jdk.VirtualThreadSubmitFailed";
+
+    private static final String LIVE_THREADS_DESCRIPTION = "Approximate current number of virtual threads that are unfinished";
+
+    private static final String METER_NAME_PREFIX = "jvm.threads.virtual.";
 
     private final RecordingStream recordingStream;
 
@@ -60,18 +68,71 @@ public class VirtualThreadMetrics implements MeterBinder, Closeable {
 
     @Override
     public void bindTo(MeterRegistry registry) {
-        Timer pinnedTimer = Timer.builder("jvm.threads.virtual.pinned")
+        Timer pinnedTimer = Timer.builder(METER_NAME_PREFIX + "pinned")
             .description("The duration while the virtual thread was pinned without releasing its platform thread")
             .tags(tags)
             .register(registry);
 
-        Counter submitFailedCounter = Counter.builder("jvm.threads.virtual.submit.failed")
+        Counter submitFailedCounter = Counter.builder(METER_NAME_PREFIX + "submit.failed")
             .description("The number of events when starting or unparking a virtual thread failed")
             .tags(tags)
             .register(registry);
 
         recordingStream.onEvent(PINNED_EVENT, event -> pinnedTimer.record(event.getDuration()));
         recordingStream.onEvent(SUBMIT_FAILED_EVENT, event -> submitFailedCounter.increment());
+
+        bindVirtualThreadSchedulerMXBean(registry);
+    }
+
+    private void bindVirtualThreadSchedulerMXBean(MeterRegistry registry) {
+        try {
+            Class clazz = Class.forName("jdk.management.VirtualThreadSchedulerMXBean");
+            PlatformManagedObject platformMXBean = ManagementFactory.getPlatformMXBean(clazz);
+            Object ignored = clazz.cast(platformMXBean);
+
+            MethodHandle getParallelism = MethodHandles.publicLookup()
+                .findVirtual(clazz, "getParallelism", MethodType.methodType(int.class));
+            MethodHandle getPoolSize = MethodHandles.publicLookup()
+                .findVirtual(clazz, "getPoolSize", MethodType.methodType(int.class));
+            MethodHandle getMountedVirtualThreadCount = MethodHandles.publicLookup()
+                .findVirtual(clazz, "getMountedVirtualThreadCount", MethodType.methodType(int.class));
+            MethodHandle getQueuedVirtualThreadCount = MethodHandles.publicLookup()
+                .findVirtual(clazz, "getQueuedVirtualThreadCount", MethodType.methodType(long.class));
+
+            Gauge.builder(METER_NAME_PREFIX + "parallelism", platformMXBean, o -> invoke(getParallelism, o))
+                .description("Virtual thread scheduler's target parallelism")
+                .register(registry);
+
+            Gauge.builder(METER_NAME_PREFIX + "pool.size", platformMXBean, o -> invoke(getPoolSize, o))
+                .baseUnit(BaseUnits.THREADS)
+                .description(
+                        "Current number of platform threads that the scheduler has started but have not terminated; -1 if not known.")
+                .register(registry);
+
+            Gauge.builder(METER_NAME_PREFIX + "live", platformMXBean, o -> invoke(getMountedVirtualThreadCount, o))
+                .tag("scheduling.status", "mounted")
+                .baseUnit(BaseUnits.THREADS)
+                .description(LIVE_THREADS_DESCRIPTION)
+                .register(registry);
+
+            Gauge.builder(METER_NAME_PREFIX + "live", platformMXBean, o -> invoke(getQueuedVirtualThreadCount, o))
+                .tag("scheduling.status", "queued")
+                .baseUnit(BaseUnits.THREADS)
+                .description(LIVE_THREADS_DESCRIPTION)
+                .register(registry);
+        }
+        catch (ClassNotFoundException | ClassCastException | NoSuchMethodException | IllegalAccessException ignored) {
+            // cannot instrument VirtualThreadSchedulerMXBean
+        }
+    }
+
+    private <T> double invoke(MethodHandle methodHandle, T arg) {
+        try {
+            return (double) methodHandle.invoke(arg);
+        }
+        catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 
     private RecordingStream createRecordingStream(RecordingConfig config) {
