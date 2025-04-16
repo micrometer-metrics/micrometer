d633e824764e5515fbc1f1aadbe5b7d986696b7c
Johnny Lim <izeye@naver.com>
2025-04-17 08:55:03 +0900

    Add Javadoc since for KeyName.withoutValue()
    Signed-off-by: Johnny Lim <izeye@naver.com>

--- micrometer-commons/src/main/java/io/micrometer/common/docs/KeyName.java
+++ micrometer-commons/src/main/java/io/micrometer/common/docs/KeyName.java
@@ -49,6 +49,7 @@ public interface KeyName {
     /**
      * Creates a key value for the given key name with {@value KeyValue#NONE_VALUE}.
      * @return key value
+     * @since 1.16.0
      */
     default KeyValue withoutValue() {
         return KeyValue.of(this, KeyValue.NONE_VALUE);
