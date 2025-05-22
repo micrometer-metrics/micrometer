dbb9577c30c8ab6e1b6200c274dac39983d8599a
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2025-05-22 12:07:19 -0700

    Polish StatsD line builders
    See gh-6270

--- implementations/micrometer-registry-statsd/src/main/java/io/micrometer/statsd/internal/TelegrafStatsdLineBuilder.java
+++ implementations/micrometer-registry-statsd/src/main/java/io/micrometer/statsd/internal/TelegrafStatsdLineBuilder.java
@@ -29,22 +29,24 @@ public class TelegrafStatsdLineBuilder extends FlavorStatsdLineBuilder {
 
     private final Object conventionTagsLock = new Object();
 
-    @SuppressWarnings({ "NullableProblems", "unused" })
     private volatile NamingConvention namingConvention;
 
-    @SuppressWarnings("NullableProblems")
     private volatile String name;
 
     @Nullable
     private volatile String conventionTags;
 
-    @SuppressWarnings("NullableProblems")
     private volatile String tagsNoStat;
 
     private final ConcurrentMap<Statistic, String> tags = new ConcurrentHashMap<>();
 
     public TelegrafStatsdLineBuilder(Meter.Id id, MeterRegistry.Config config) {
         super(id, config);
+
+        this.namingConvention = config.namingConvention();
+        this.conventionTags = createConventionTags(namingConvention);
+        this.name = createName(namingConvention);
+        this.tagsNoStat = createTagsNoStat(conventionTags);
     }
 
     @Override
@@ -61,15 +63,28 @@ public class TelegrafStatsdLineBuilder extends FlavorStatsdLineBuilder {
                     return;
                 }
                 this.tags.clear();
-                this.conventionTags = id.getTagsAsIterable().iterator().hasNext() ? id.getConventionTags(next)
-                    .stream()
-                    .map(t -> telegrafEscape(t.getKey()) + "=" + telegrafEscape(t.getValue()))
-                    .collect(Collectors.joining(",")) : null;
+                this.conventionTags = createConventionTags(next);
             }
-            this.name = telegrafEscape(next.name(id.getName(), id.getType(), id.getBaseUnit()));
-            this.tagsNoStat = tags(null, conventionTags, "=", ",");
+            this.name = createName(next);
+            this.tagsNoStat = createTagsNoStat(conventionTags);
             this.namingConvention = next;
         }
+    }
+
+    @Nullable
+    private String createConventionTags(NamingConvention namingConvention) {
+        return id.getTagsAsIterable().iterator().hasNext() ? id.getConventionTags(namingConvention)
+            .stream()
+            .map(t -> telegrafEscape(t.getKey()) + "=" + telegrafEscape(t.getValue()))
+            .collect(Collectors.joining(",")) : null;
+    }
+
+    private String createName(NamingConvention namingConvention) {
+        return telegrafEscape(namingConvention.name(id.getName(), id.getType(), id.getBaseUnit()));
+    }
+
+    private String createTagsNoStat(@Nullable String conventionTags) {
+        return tags(null, conventionTags, "=", ",");
     }
 
     private String tagsByStatistic(@Nullable Statistic stat) {
