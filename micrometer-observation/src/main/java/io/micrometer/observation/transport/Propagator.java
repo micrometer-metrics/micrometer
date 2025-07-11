c17908a63ac8622d198ca0ca0fd11eb425f71c54
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-07-11 15:13:54 +0900

    Add getAll to Getter for repeated metadata (#6312)
    It's known that some metadata can be repeated on a carrier. W3C baggage is one example of this in HTTP request headers. The current get API returns a String, and for performance reasons we may not want to check all headers to combine repeated headers when we don't expect there should be repeated headers.
    
    This updates the one non-deprecated main code implementation of Getter (in ObservationGrpcServerInterceptor) to implement the new getAll method.

--- micrometer-observation/src/main/java/io/micrometer/observation/transport/Propagator.java
+++ micrometer-observation/src/main/java/io/micrometer/observation/transport/Propagator.java
@@ -17,6 +17,8 @@ package io.micrometer.observation.transport;
 
 import org.jspecify.annotations.Nullable;
 
+import java.util.Collections;
+
 /**
  * Inspired by OpenZipkin Brave and OpenTelemetry. Most of the documentation is taken
  * directly from OpenTelemetry.
@@ -75,13 +77,39 @@ public interface Propagator {
 
         /**
          * Returns the first value of the given propagation {@code key} or returns
-         * {@code null}.
+         * {@code null}. This method should be preferred over
+         * {@link #getAll(Object, String)}} for better performance in cases where it is
+         * expected that the key is not repeated.
          * @param carrier carrier of propagation fields, such as an http request.
          * @param key the key of the field.
          * @return the first value of the given propagation {@code key} or returns
          * {@code null}.
          */
         @Nullable String get(C carrier, String key);
+
+        /**
+         * Get all values of the given propagation {@code key}, if any exist. This should
+         * only be used when it is expected that the key may be repeated.
+         * {@link #get(Object, String)} should be preferred in other cases for
+         * performance.
+         * @param carrier carrier of propagation fields, such as an http request.
+         * @param key the key of the field.
+         * @return all values of the given propagation {@code key} or returns an empty
+         * {@code Iterable} if no values are found.
+         * @implNote For backward-compatibility, a default implementation is provided that
+         * returns a list with the value of {@link #get(Object, String)} or an empty list
+         * if no values are found. Implementors of this interface should override this
+         * method to provide an implementation that returns all present values of the
+         * given propagation {@code key}.
+         * @since 1.16.0
+         */
+        default Iterable<String> getAll(C carrier, String key) {
+            String firstValue = get(carrier, key);
+            if (firstValue == null) {
+                return Collections.emptyList();
+            }
+            return Collections.singletonList(firstValue);
+        }
 
     }
 
