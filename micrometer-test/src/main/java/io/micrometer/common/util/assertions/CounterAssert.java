a7cd32f6d768bacb94986fd3c81187505b596ca6
etrandafir93 <emanueltrandafir1993@gmail.com>
2025-10-25 15:34:31 +0300

    Add Assertions for various Meters
    Closes gh-6763
    
    Signed-off-by: etrandafir93 <emanueltrandafir1993@gmail.com>

---
+++ micrometer-test/src/main/java/io/micrometer/common/util/assertions/CounterAssert.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2025 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.common.util.assertions;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Tag;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.IntegerAssert;
+
+/**
+ * Assertion methods for {@link Counter}s.
+ * <p>
+ * To create a new instance of this class, invoke
+ * {@link CounterAssert#assertThat(Counter)} or use
+ * {@link io.micrometer.core.tck.MeterRegistryAssert#counter(String, Tag...)}.
+ *
+ * @author Emanuel Trandafir
+ */
+public class CounterAssert extends AbstractAssert<CounterAssert, Counter> {
+
+    /**
+     * Creates a new instance of {@link CounterAssert}.
+     * @param actual the counter to assert on
+     * @return the created assertion object
+     */
+    public static CounterAssert assertThat(Counter actual) {
+        return new CounterAssert(actual);
+    }
+
+    /**
+     * Creates a new instance of {@link CounterAssert}.
+     * @param actual the counter to assert on
+     */
+    public CounterAssert(Counter actual) {
+        super(actual, CounterAssert.class);
+    }
+
+    /**
+     * Verifies that the counter's count is equal to the expected count.
+     * <p>
+     * Example: <pre><code class='java'>
+     * Counter counter = Counter.builder("my.counter").register(registry);
+     * counter.increment();
+     * counter.increment();
+     *
+     * assertThat(counter).hasCount(2);
+     * </code></pre>
+     * @param expected the expected count
+     * @return this assertion object for chaining
+     * @see #count()
+     */
+    public CounterAssert hasCount(int expected) {
+        count().isEqualTo(expected);
+        return this;
+    }
+
+    /**
+     * Returns AssertJ's {@link IntegerAssert} for the counter's current count.
+     * <p>
+     * Example: <pre><code class='java'>
+     * Counter counter = Counter.builder("my.counter").register(registry);
+     * counter.increment(5);
+     *
+     * assertThat(counter).count()
+     *     .isBetween(1, 10)
+     *     .isGreaterThan(3);
+     * </code></pre>
+     * @return an {@link IntegerAssert} for further assertions
+     * @see #hasCount(int)
+     */
+    public IntegerAssert count() {
+        return new IntegerAssert((int) actual.count());
+    }
+
+}
---
+++ micrometer-test/src/test/java/io/micrometer/common/util/assertions/MeterAssertTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.common.util.assertions;
+
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Statistic;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.micrometer.core.tck.MeterRegistryAssert;
+import org.junit.jupiter.api.Test;
+
+class MeterAssertTest {
+
+    SimpleMeterRegistry simpleMeterRegistry = new SimpleMeterRegistry();
+
+    MeterRegistryAssert meterRegistryAssert = MeterRegistryAssert.assertThat(simpleMeterRegistry);
+
+    @Test
+    void shouldAssertOnMeasures() {
+        DistributionSummary meter = DistributionSummary.builder("foo").register(simpleMeterRegistry);
+
+        meter.record(10.0);
+        meter.record(20.0);
+
+        meterRegistryAssert.meter("foo")
+            .hasMeasurement(Statistic.COUNT, 2.0)
+            .hasMeasurement(Statistic.TOTAL, 30.0)
+            .hasMeasurement(Statistic.MAX, 20.0);
+    }
+
+    @Test
+    void shouldAssertOnType() {
+        DistributionSummary.builder("foo").register(simpleMeterRegistry).record(100.0);
+
+        meterRegistryAssert.meter("foo").hasType(Meter.Type.DISTRIBUTION_SUMMARY);
+    }
+
+}
