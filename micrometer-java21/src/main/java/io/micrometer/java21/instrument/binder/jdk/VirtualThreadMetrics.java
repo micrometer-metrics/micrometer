086a668a680bf55ee3703ce7fc9416787a68b6a1
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-12-03 15:24:41 +0900

    Fix javadoc errors

--- micrometer-java21/src/main/java/io/micrometer/java21/instrument/binder/jdk/VirtualThreadMetrics.java
+++ micrometer-java21/src/main/java/io/micrometer/java21/instrument/binder/jdk/VirtualThreadMetrics.java
@@ -36,7 +36,7 @@ import static java.util.Collections.emptyList;
  *
  * @author Artyom Gabeev
  * @see <a href="https://openjdk.org/jeps/425#JDK-Flight-Recorder-JFR">JEP 425</a>
- * @see jdk.management.VirtualThreadSchedulerMXBean
+ * @see <a href="https://www.oracle.com/java/technologies/javase/24-relnote-issues.html#JDK-8338890">jdk.management.VirtualThreadSchedulerMXBean</a>
  * @since 1.14.0
  */
 public class VirtualThreadMetrics implements MeterBinder, Closeable {
