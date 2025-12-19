a3b5ba13352a28c1fe50d4da426dc93dd0ee5804
allnightlong <allnightlong@allnightlong.ru>
2025-12-19 16:21:32 +0400

    CompressionMode config variable
    Signed-off-by: Tigran Kavanosyan <allnightlong@allnightlong.ru>

---
+++ implementations/micrometer-registry-otlp/src/main/java/io/micrometer/registry/otlp/CompressionMode.java
@@ -0,0 +1,13 @@
+package io.micrometer.registry.otlp;
+
+/**
+ * Compression mode for metric data sender.
+ *
+ * @author Tigran Kavanosyan
+ * @since 1.17.0
+ */
+public enum CompressionMode {
+
+    ON, OFF
+
+}
