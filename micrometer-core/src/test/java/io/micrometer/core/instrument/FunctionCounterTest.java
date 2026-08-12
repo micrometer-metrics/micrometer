25ed5733294b7f60239a31d3949154de68c66486
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2026-08-12 17:03:38 +0900

    More tests

--- micrometer-core/src/test/java/io/micrometer/core/instrument/FunctionCounterTest.java
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/FunctionCounterTest.java
@@ -61,4 +61,69 @@ class FunctionCounterTest {
         assertThat(functionCounter.getId().getBaseUnit()).isEqualTo("connections");
     }
 
+    @Test
+    void tagsAndDescriptionPreservedForTimeBasedCounter() {
+        FunctionCounter functionCounter = FunctionCounter
+            .builder("my.time.counter", new AtomicInteger(), AtomicInteger::doubleValue, TimeUnit.MILLISECONDS)
+            .tags("env", "prod")
+            .tag("region", "us-east")
+            .description("Time-based counter description")
+            .register(registry);
+
+        assertThat(functionCounter.getId().getTag("env")).isEqualTo("prod");
+        assertThat(functionCounter.getId().getTag("region")).isEqualTo("us-east");
+        assertThat(functionCounter.getId().getDescription()).isEqualTo("Time-based counter description");
+    }
+
+    @Test
+    void scalesToDifferentRegistryBaseUnits() {
+        MeterRegistry millisRegistry = new SimpleMeterRegistry() {
+            @Override
+            protected TimeUnit getBaseTimeUnit() {
+                return TimeUnit.MILLISECONDS;
+            }
+        };
+
+        AtomicLong state = new AtomicLong(5000);
+
+        FunctionCounter secondsCounter = FunctionCounter
+            .builder("my.time.counter", state, AtomicLong::doubleValue, TimeUnit.MILLISECONDS)
+            .register(registry);
+
+        FunctionCounter millisCounter = FunctionCounter
+            .builder("my.time.counter", state, AtomicLong::doubleValue, TimeUnit.MILLISECONDS)
+            .register(millisRegistry);
+
+        assertThat(secondsCounter.count()).isEqualTo(5.0);
+        assertThat(millisCounter.count()).isEqualTo(5000.0);
+        assertThat(millisCounter.getId().getBaseUnit()).isEqualTo("milliseconds");
+    }
+
+    @Test
+    void reRegistrationReturnsExistingInstance() {
+        AtomicLong state1 = new AtomicLong(100);
+        AtomicLong state2 = new AtomicLong(200);
+
+        FunctionCounter counter1 = FunctionCounter
+            .builder("my.time.counter", state1, AtomicLong::doubleValue, TimeUnit.MILLISECONDS)
+            .register(registry);
+
+        FunctionCounter counter2 = FunctionCounter
+            .builder("my.time.counter", state2, AtomicLong::doubleValue, TimeUnit.MILLISECONDS)
+            .register(registry);
+
+        assertThat(counter2).isSameAs(counter1);
+        assertThat(counter2.count()).isEqualTo(0.1);
+    }
+
+    @Test
+    void handlesFractionalTimeValues() {
+        AtomicLong micros = new AtomicLong(1500);
+        FunctionCounter counter = FunctionCounter
+            .builder("my.time.counter", micros, AtomicLong::doubleValue, TimeUnit.MICROSECONDS)
+            .register(registry);
+
+        assertThat(counter.count()).isEqualTo(0.0015);
+    }
+
 }
